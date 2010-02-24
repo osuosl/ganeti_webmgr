aa1e5cdce3d02be39243ef832db07d18a56b20a8
Peter Krenesky <peter@osuosl.org>
2010-02-24 07:40:38 -0800

    adding settings for tests

---
+++ test/settings.py
@@ -0,0 +1,112 @@
+# Django settings for maintain project.
+
+VERSION = '0.0001'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'test_database'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+CACHE_BACKEND = 'locmem://'
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+
+# absolute path to the docroot of this site.  Detect automatically.
+DOC_ROOT = __file__[__file__.rfind('/')]
+
+
+# prefix used for the site.  ie. http://myhost.com/<SITE_ROOT>/
+# for the django standalone server this should be ""
+ROOT_URL = ''
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = '%s/static' % DOC_ROOT
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '%s/static' % ROOT_URL
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '560j+p&_tnn7czy$-8f$zq!u@1b6#67(4r$l2)ru%*z5!&9-+a'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.sites',
+    #'django.contrib.admin',
+    'muddle',
+    'test_app',
+)
+
+LOGIN_REDIRECT_URL = '/plugins'
+
+AUTH_PROFILE_MODULE = 'muddle.UserProfile'
+
+
+
+# core plugins - these are plugins that are loaded before all other plugins
+# because they provide some core functionality.  They can only be disabled by
+# changing this setting.
+CORE_PLUGINS = (
+    'muddle.plugins.managers.type_manager.TypeManager',
+    'muddle.plugins.managers.view_manager.ViewManager',
+    'muddle.plugins.model_support.ModelManager',
+    'muddle.plugins.permissions.PermissionsPlugin'
+)
