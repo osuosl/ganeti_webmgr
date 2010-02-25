f7e28472d3b9d982461e50351d2daa7a4786163c
Peter Krenesky <peter@osuosl.org>
2010-02-25 07:00:05 -0800

    MEDIA root is now static, relative to siteroot

--- muddle/__init__.py
+++ muddle/__init__.py
@@ -3,14 +3,17 @@ from django.conf import settings
 from muddle.plugins.registerable import PERM_ALL, PERM_NONE, PERM_READ, \
                                     PERM_WRITE, PERM_CREATE, PERM_DELETE
 
+VERSION = '0.0.1'
+MUDDLE_ROOT = __file__[__file__.rfind('/')]
+
 def settings_processor(request):
     """
     settings_processor adds settings required by most pages
     """
     return {
-        'VERSION':settings.VERSION,
-        'MEDIA':settings.MEDIA_URL,
-        'ROOT':settings.ROOT_URL
+        'VERSION':VERSION,
+        'MEDIA':'%s/muddle_static' % settings.SITE_ROOT,
+        'ROOT':settings.SITE_ROOT
     }
 
 
