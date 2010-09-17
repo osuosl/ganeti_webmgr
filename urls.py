5d2961be374519cc7fdd8082177c3f3940784d83
Trevor Bramwell <bramwelt@osuosl.org>
2010-09-17 15:22:18 -0700

    Updated the admin url to not match EVERYTHING. Updated setting.py.dist to allow easy indication of base directory of ganeti_webmgr

--- urls.py
+++ urls.py
@@ -39,5 +39,5 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include(admin.site.urls)),
 )
