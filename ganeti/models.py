7baa84a6a1a48da7a790ee9af62fc837a3681b6d
Trevor Bramwell <bramwelt@osuosl.org>
2010-09-22 13:07:34 -0700

    Removed unneeded and obsolete methods. Added Cluster methods to get a node, all nodes, a instance, and all instances.

--- ganeti/models.py
+++ ganeti/models.py
@@ -1,30 +1,11 @@
 import cPickle
-import os
-import sys
-import urllib
-import urllib2
 
 from django.db import models
 from django.contrib.auth.models import User, Group
-from simplejson import JSONEncoder, JSONDecoder
-from time import sleep
 from ganeti_webmgr.util import client
-from ganeti_webmgr.util.portforwarder import forward_port
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from datetime import datetime
 
-dec = JSONDecoder()
 curl = client.GenericCurlConfig()
-
-
-class MethodRequest(urllib2.Request):
-    def __init__(self, method, *args, **kwargs):
-        self._method = method
-        urllib2.Request.__init__(self, *args, **kwargs)
-
-    def get_method(self):
-        return self._method
-
 
 class VirtualMachine(models.Model):
     """
@@ -171,54 +152,7 @@ class Cluster(models.Model):
     def __unicode__(self):
         return self.hostname
 
-    def _get_resource(self, resource, method='GET', data=None):
-        # Strip trailing slashes, as ganeti-rapi doesn't like them
-        resource = resource.rstrip('/')
-
-        # create a password manager
-        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-
-        # Add the username and password.
-        # If we knew the realm, we could use it instead of ``None``.
-        top_level_url = 'https://%s:%d/2/' % (self.hostname, self.port)
-        password_mgr.add_password(None, top_level_url,
-                                  self.username, self.password)
-
-        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-
-        # create "opener" (OpenerDirector instance)
-        opener = urllib2.build_opener(handler)
-
-        # Install the opener.
-        # Now all calls to urllib2.urlopen use our opener.
-        urllib2.install_opener(opener)
-
-        req = MethodRequest(method, 'https://%s:%d%s' %
-                            (self.hostname, self.port, resource),
-                            data=data)
-        response = urllib2.urlopen(req)
-        if response.code != 200:
-            raise ValueError("'%s' is not a valid resource" % resource)
-        try:
-            contenttype = response.info()['Content-Type']
-        except:
-            contenttype = None
-
-        if contenttype != 'application/json':
-            raise ValueError("Invalid response type '%s'" % contenttype)
-
-        return dec.decode(response.read())
-    """
-    def get_instance(self, name):
-        for inst in self.get_instances():
-            if inst.name == name:
-                return inst
-        return None
-
-    def get_instances(self):
-        return [ Instance(self, info['name'], info) for info in self.get_cluster_instances_detail() ]
-    """
-    def get_cluster_info(self):
+    def info(self):
         info = self.rapi.GetInfo()
         #print info['ctime']
         if 'ctime' in info and info['ctime']:
@@ -227,53 +161,17 @@ class Cluster(models.Model):
             info['mtime'] = datetime.fromtimestamp(info['mtime'])
         return info
 
-    def get_cluster_nodes(self):
+    def nodes(self):
         return self.rapi.GetNodes()
+
+    def node(self, node):
+        return self.rapi.GetNode(node)
 
     def instances(self):
         return self.rapi.GetInstances()
 
-    def get_cluster_instances(self):
-        return self.rapi.GetInstances(bulk=False)
-
-    def get_cluster_instances_detail(self):
-        return self.rapi.GetInstances(bulk=True)
-
-    def get_node_info(self, node):
-        return self.rapi.GetNode(node)
-
-    def get_instance_info(self, instance):
-        return self.rapi.GetInstance(instance.strip())
-    """
-    def set_random_vnc_password(self, instance):
-        jobid = self._get_resource('/2/instances/%s/randomvncpass' %
-                                   instance.strip(),
-                                   method="POST")
-        tries = 0
-        jobinfo = {}
-        while tries < 10:
-            jobinfo = self._get_resource('/2/jobs/%s' % jobid)
-            if jobinfo['status'] == "error":
-                return None
-            elif jobinfo['status'] == "success":
-                break
-            tries += 1
-            sleep(0.5)
-        if jobinfo:
-            return jobinfo['opresult'][0]
-        else:
-            return None
-
-    def setup_vnc_forwarding(self, instance):
-        password = self.set_random_vnc_password(instance)
-        info = self.get_instance_info(instance)
-
-        port = info['network_port']
-        node = info['pnode']
-
-        os.system("portforwarder.py %d %s:%d" % (port, node, port))
-        return (port, password)
-    """
+    def instance(self, instance):
+        return self.rapi.GetInstance(instance)
 
 
 class ClusterUser(models.Model):
