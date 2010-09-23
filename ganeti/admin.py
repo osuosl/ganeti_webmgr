2b88b90760a67b0d1188891ff3f4cf41e40e2edf
Peter Krenesky <peter@osuosl.org>
2010-09-23 10:06:00 -0700

    Updating admin to remove quota, moving it as an inline for the objects that have a relationship through it.

--- ganeti/admin.py
+++ ganeti/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from models import *
 
-admin.site.register(Cluster)
+
+class QuotaInline(admin.TabularInline):
+    model = Quota
+    extra = 1
+
+class ClusterAdmin(admin.ModelAdmin):
+    inlines = [QuotaInline]
+
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [QuotaInline]
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [QuotaInline]
+
+admin.site.register(Cluster, ClusterAdmin)
 admin.site.register(VirtualMachine)
-admin.site.register(Organization)
-admin.site.register(Profile)
-admin.site.register(Quota)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Profile, ProfileAdmin)
