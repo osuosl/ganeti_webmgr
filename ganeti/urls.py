c13d733585a2a1e139f08fb1a082d232872336e2
Trevor Bramwell <bramwelt@osuosl.org>
2010-09-29 17:53:38 -0700

    Updated URLS to use just view name by adding reference to first item of patterns

--- ganeti/urls.py
+++ ganeti/urls.py
@@ -3,46 +3,57 @@ from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
 from ganeti_webmgr.ganeti.models import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns('ganeti_webmgr.ganeti.views',
     # Example:
     # (r'^ganeti_webmgr/', include('ganeti_webmgr.foo.urls')),
     url(r'^/?$',
-        'ganeti_webmgr.ganeti.views.index', name="cluster-overview"),
-    # Cluster Views
+        'index', name="cluster-overview"),
+        
+    # Cluster
+    #   List
+    url(r'^clusters/',
+        'cluster_list', name="cluster-list"),
+    #   Detail
     url(r'^cluster/(?P<cluster_slug>\w+)/?$',
-        'ganeti_webmgr.ganeti.views.cluster_detail', name="cluster-detail"),
-    url(r'^cluster/(?P<cluster_slug>\w+)/create/?$',
-        'ganeti_webmgr.ganeti.views.create', name="instance-create"),
-    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/vnc/?$',
-        'ganeti_webmgr.ganeti.views.vnc', name="instance-vnc"),
-    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/shutdown/?$',
-        'ganeti_webmgr.ganeti.views.shutdown', name="instance-shutdown"),
-    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/startup/?$',
-        'ganeti_webmgr.ganeti.views.startup', name="instance-startup"),
-    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/reboot/?$',
-        'ganeti_webmgr.ganeti.views.reboot', name="instance-reboot"),
+        'cluster_detail', name="cluster-detail"),
+    # Instance
+    #  List
+    #  Detail
     url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/?',
-        'ganeti_webmgr.ganeti.views.instance', name="instance-detail"),
-    url(r'^clusters/', 'ganeti_webmgr.ganeti.views.cluster_list',
-        name="cluster-list"),
-    
-    # Organizations
-    url(r'^organization/(?P<id>\d+)/?$',
-        'ganeti_webmgr.ganeti.organizations.detail', name="organization-detail"),
-    url(r'^organization/(?P<id>\d+)/user/add/?$',
-        'ganeti_webmgr.ganeti.organizations.add_user', name="organization-add-user"),
-    url(r'^organization/(?P<id>\d+)/user/remove/?$',
-        'ganeti_webmgr.ganeti.organizations.remove_user', name="organization-remove-user"),
-    url(r'^organization/(?P<id>\d+)/user/update/?$',
-        'ganeti_webmgr.ganeti.organizations.update_user', name="organization-update-user"),
-    
-    # Admin
-    url(r'^orphans/', 'ganeti_webmgr.ganeti.views.orphans',
-        name='instance-orphans'),
+        'instance', name="instance-detail"),
+    #  Create
+    url(r'^cluster/(?P<cluster_slug>\w+)/create/?$',
+        'create', name="instance-create"),
+    #  Start, Stop, Reboot
+    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/vnc/?$',
+        'vnc', name="instance-vnc"),
+    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/shutdown/?$',
+        'shutdown', name="instance-shutdown"),
+    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/startup/?$',
+        'startup', name="instance-startup"),
+    url(r'^cluster/(?P<cluster_slug>\w+)/(?P<instance>[^/]+)/reboot/?$',
+        'reboot', name="instance-reboot"),
+    #   Orphans
+    url(r'^orphans/',
+        'orphans', name='instance-orphans'),
     
     # Authentication
-    url(r'^user/login/?', 'ganeti_webmgr.ganeti.views.login_view', name="login"),
-    url(r'^user/logout/?', 'ganeti_webmgr.ganeti.views.logout_view', name="logout"),
+    url(r'^user/login/?',
+        'login_view', name="login"),
+    url(r'^user/logout/?',
+        'logout_view', name="logout"),
+)
+
+urlpatterns += patterns('ganeti_webmgr.ganeti.organizations',
+    # Organizations
+    url(r'^organization/(?P<id>\d+)/?$',
+        'detail', name="organization-detail"),
+    url(r'^organization/(?P<id>\d+)/user/add/?$',
+        'add_user', name="organization-add-user"),
+    url(r'^organization/(?P<id>\d+)/user/remove/?$',
+        'remove_user', name="organization-remove-user"),
+    url(r'^organization/(?P<id>\d+)/user/update/?$',
+        'update_user', name="organization-update-user"),
 )
 
 if settings.DEBUG:
