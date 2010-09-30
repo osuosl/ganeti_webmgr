69d5f751c7890b805f54ef253605ca35846f1ef9
Peter Krenesky <peter@osuosl.org>
2010-09-30 09:37:21 -0700

    Organizations - added view handlers, and templates (some stubs) for editing users within an organization.  tests included

--- ganeti/organizations.py
+++ ganeti/organizations.py
@@ -3,11 +3,12 @@ import json
 from django import forms
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect, \
+    HttpResponseForbidden, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404, render_to_response
 
 from ganeti.models import Organization
-
+from object_permissions import get_model_perms, grant
 
 def detail(request, id):
     """
@@ -16,12 +17,48 @@ def detail(request, id):
     #TODO permission check
     
     org = get_object_or_404(Organization, id=id)
-    return render_to_response("organization.html", {'org':org})
+    return render_to_response("organizations/detail.html", {'org':org})
 
 
-class AddUserForm(forms.Form):
+class UserForm(forms.Form):
+    """
+    Base form for dealing with users
+    """
     user = forms.ModelChoiceField(queryset=User.objects.all())
+    organization = None
 
+    def __init__(self, organization=None, *args, **kwargs):
+        self.organization=organization
+        super(UserForm, self).__init__(*args, **kwargs)
+
+
+class AddUserForm(UserForm):
+    def clean_user(self):
+        """ Validate that user is not in organization already """
+        user = self.cleaned_data['user']
+        if self.organization.users.filter(user=user).exists():
+            raise forms.ValidationError("User is already a member of this group")
+        return user
+
+
+class RemoveUserForm(UserForm):
+    def clean_user(self):
+        """ Validate that user is in organization """
+        user = self.cleaned_data['user']
+        if not self.organization.users.filter(user=user).exists():
+            raise forms.ValidationError("User is not a member of this group")
+        return user
+
+
+class UserPermissionForm(UserForm):
+    """
+    Form used for editing permissions
+    """
+    permissions = forms.MultipleChoiceField()
+
+    def __init__(self, choices=[], *args, **kwargs):
+        super(UserPermissionForm, self).__init__(*args, **kwargs)
+        self.fields['permissions'].choices = choices
 
 def add_user(request, id):
     """
@@ -31,17 +68,16 @@ def add_user(request, id):
     organization = get_object_or_404(Organization, id=id)
     
     if not (user.is_superuser or user.has_perm('admin', organization)):
-        print user.has_perm('admin', organization)
         return HttpResponseForbidden('You do not have sufficient privileges')
     
     if request.method == 'POST':
-        form = AddUserForm(request.POST)
+        form = AddUserForm(organization, request.POST)
         if form.is_valid():
-            user_id = form.cleaned_data['user']
-            user = User.objects.get(id=user_id)
+            user = form.cleaned_data['user']
+            organization.users.add(user.get_profile())
             
             # return html for new user row
-            return render_to_response("user_row.html", {'user':user})
+            return render_to_response("organizations/user_row.html", {'user':user})
         
         # error in form return ajax response
         content = json.dumps(form.errors)
@@ -61,13 +97,48 @@ def remove_user(request, id):
     if not (user.is_superuser or user.has_perm('admin', organization)):
         return HttpResponseForbidden('You do not have sufficient privileges')
     
-    if request.method == 'GET':
+    if request.method != 'POST':
         return HttpResponseNotAllowed('GET')
 
+    form = RemoveUserForm(organization, request.POST)
+    if form.is_valid():
+        user = form.cleaned_data['user']
+        organization.users.remove(user.get_profile())
+        
+        # return success
+        return HttpResponse('1', mimetype='application/json')
+        
+    # error in form return ajax response
+    content = json.dumps(form.errors)
+    return HttpResponse(content, mimetype='application/json')
 
-def update_user(request):
+
+def update_user(request, id):
     """
     Ajax call to update a user's permissions
     """
-    #TODO permission check
-    pass
+    user = request.user
+    organization = get_object_or_404(Organization, id=id)
+    perms = get_model_perms(organization)
+    choices = zip(perms, perms)
+    
+    if not (user.is_superuser or user.has_perm('admin', organization)):
+        return HttpResponseForbidden('You do not have sufficient privileges')
+    
+    if request.method == 'POST':
+        form = UserPermissionForm(choices, organization, request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
+            perms = form.cleaned_data['permissions']
+            for perm in perms:
+                grant(user, perm, organization)
+            
+            # return html to replace existing user row
+            return render_to_response("organizations/user_row.html", {'user':user})
+        
+        # error in form return ajax response
+        content = json.dumps(form.errors)
+        return HttpResponse(content, mimetype='application/json')
+
+    form = UserPermissionForm(choices)
+    return render_to_response("organizations/permissions.html", {'form':form})
--- ganeti/tests/organizations.py
+++ ganeti/tests/organizations.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
 
-from object_permissions import register, grant, get_user_perms, get_model_perms
+from object_permissions import register, grant, revoke, get_user_perms, \
+    get_model_perms
 from object_permissions.models import ObjectPermission
 
 from ganeti.models import Organization
@@ -74,7 +75,7 @@ class TestOrganizations(TestCase):
             * POST with a user id adds user, response is html for user
             * POST without user id returns error as json
             * POST for invalid user id returns error as json
-            * adding user a second time does not cause error or duplicate
+            * adding user a second time returns error as json
         """
         user = self.user
         org = self.test_save()
@@ -86,27 +87,46 @@ class TestOrganizations(TestCase):
         response = c.post('/organization/%d/user/add/' % org.id)
         self.assertEqual(403, response.status_code)
         
-        # authorized post (create form)
+        # authorized post (perm granted)
         grant(user, 'admin', org)
         self.assert_(c.login(username=user.username, password='secret'))
         response = c.get('/organization/%d/user/add/' % org.id)
         self.assertEqual(200, response.status_code)
+        self.assertEquals('text/html; charset=utf-8', response['content-type'])
+        self.assertEqual('organizations/add_user.html', response.template.name)
+        
+        # authorized post (superuser)
+        revoke(user, 'admin', org)
+        user.is_superuser = True
+        user.save()
+        response = c.get('/organization/%d/user/add/' % org.id)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('text/html; charset=utf-8', response['content-type'])
+        self.assertEqual('organizations/add_user.html', response.template.name)
         
         # missing user id
+        response = c.post('/organization/%d/user/add/' % org.id)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
         
         # invalid user
+        response = c.post('/organization/%d/user/add/' % org.id, {'user':0})
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
         
         # valid post
         data = {'user':user.id}
         response = c.post('/organization/%d/user/add/' % org.id, data)
         self.assertEqual(200, response.status_code)
-        self.assert_()
+        self.assertEquals('text/html; charset=utf-8', response['content-type'])
+        self.assertEqual('organizations/user_row.html', response.template.name)
+        self.assert_(org.users.filter(user=user).exists())
         
-        # same user again        
+        # same user again
         response = c.post('/organization/%d/user/add/' % org.id, data)
         self.assertEqual(200, response.status_code)
-        
-        print response.__dict__
+        self.assertEquals('application/json', response['content-type'])
+        self.assertEquals(org.users.filter(user=user).count(), 1)
     
     def test_view_remove_user(self):
         """
@@ -116,20 +136,127 @@ class TestOrganizations(TestCase):
             * GET redirects user to 405
             * POST with a user id remove user, returns 1
             * POST without user id returns error as json
-            * user lacking "admin" perm on this org is redirected
+            * users lacking perms receive 403
+            * removing user not in group returns error as json
+            * removing user that does not exist returns error as json
         """
         user = self.user
         org = self.test_save()
         c = Client()
+        org.users.add(user.get_profile())
+        
+        # invalid permissions
+        response = c.get('/organization/%d/user/add/' % org.id)
+        self.assertEqual(403, response.status_code)
+        response = c.post('/organization/%d/user/add/' % org.id)
+        self.assertEqual(403, response.status_code)
+        
+        # authorize and login
+        self.assert_(c.login(username=user.username, password='secret'))
+        grant(user, 'admin', org)
         
         # invalid method
         response = c.get('/organization/%d/user/remove/' % org.id)
         self.assertEqual(405, response.status_code)
         
-        response = c.post('/organization/%d/user/remove/' % org.id)
+        # valid request (perm)
+        data = {'user':user.id}
+        response = c.post('/organization/%d/user/remove/' % org.id, data)
         self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        self.assertEqual('1', response.content)
+        self.assertFalse(org.users.filter(user=user).exists())
         
-        c.login(username=user.username, password=user.password)
+        # valid request (superuser)
+        revoke(user, 'admin', org)
+        user.is_superuser = True
+        user.save()
+        org.users.add(user.get_profile())
+        response = c.post('/organization/%d/user/remove/' % org.id, data)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        self.assertEqual('1', response.content)
+        self.assertFalse(org.users.filter(user=user).exists())
+        
+        # remove user again
+        response = c.post('/organization/%d/user/remove/' % org.id, data)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        self.assertFalse(org.users.filter(user=user).exists())
+        self.assertNotEqual('1', response.content)
+        
+        # remove invalid user
+        response = c.post('/organization/%d/user/remove/' % org.id, {'user':0})
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        self.assertNotEqual('1', response.content)
     
     def test_view_update_permissions(self):
-        raise NotImplementedError
+        """
+        Tests setting permissions for a user
+        
+        Verifies:
+            * request from unauthorized user results in 403
+            * GET returns a 200 code, response is html
+            * POST with a user id adds user, response is html for user
+            * POST without user id returns error as json
+            * POST for invalid user id returns error as json
+            * adding user a second time returns error as json
+        """
+        user = self.user
+        org = self.test_save()
+        org.users.add(user.get_profile())
+        
+        register('Perm1', Organization)
+        register('Perm2', Organization)
+        
+        c = Client()
+        
+        # unauthorized
+        response = c.get('/organization/%d/user/update/' % org.id)
+        self.assertEqual(403, response.status_code)
+        response = c.post('/organization/%d/user/update/' % org.id)
+        self.assertEqual(403, response.status_code)
+        
+        # authorized post (perm granted)
+        grant(user, 'admin', org)
+        self.assert_(c.login(username=user.username, password='secret'))
+        response = c.get('/organization/%d/user/update/' % org.id)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('text/html; charset=utf-8', response['content-type'])
+        self.assertEqual('organizations/permissions.html', response.template.name)
+        
+        # authorized post (superuser)
+        revoke(user, 'admin', org)
+        user.is_superuser = True
+        user.save()
+        response = c.get('/organization/%d/user/update/' % org.id)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('text/html; charset=utf-8', response['content-type'])
+        self.assertEqual('organizations/permissions.html', response.template.name)
+        
+        # missing user id
+        response = c.post('/organization/%d/user/update/' % org.id)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        
+        # invalid user
+        response = c.post('/organization/%d/user/update/' % org.id, {'user':0})
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        
+        # invalid permission
+        data = {'user':user.id, 'permissions':['DoesNotExist']}
+        response = c.post('/organization/%d/user/update/' % org.id, data)
+        self.assertEqual(200, response.status_code)
+        self.assertEquals('application/json', response['content-type'])
+        
+        # valid post
+        data = {'user':user.id, 'permissions':['Perm1','Perm2']}
+        response = c.post('/organization/%d/user/update/' % org.id, data)
+        self.assertEqual(200, response.status_code)
+        
+        self.assertEquals('text/html; charset=utf-8', response['content-type'])
+        self.assertEqual('organizations/user_row.html', response.template.name)
+        self.assert_(user.has_perm('Perm1', org))
+        self.assert_(user.has_perm('Perm2', org))
