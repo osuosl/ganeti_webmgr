0956f40ad11af93e30ea2826a05e4dfc9aefa7e6
Peter Krenesky <peter@osuosl.org>
2010-10-04 15:42:36 -0700

    Add form validation for new users

--- object_permissions/views/permissions.py
+++ object_permissions/views/permissions.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from object_permissions import get_model_perms, grant, revoke
+from object_permissions import get_user_perms, get_model_perms, grant, revoke
 
 
 class ObjectPermissionForm(forms.Form):
@@ -38,3 +38,34 @@ class ObjectPermissionForm(forms.Form):
         for perm in [p for p in model_perms if p not in perms]:
             revoke(user, perm, object)
         return perms
+
+
+class ObjectPermissionFormNewUsers(ObjectPermissionForm):
+    """
+    A subclass of permission form that enforces an addtional rule that new users
+    must be granted at least one permission.  This is used for objects that
+    determine group membership (e.g. listing users with acccess) based on who
+    has permissions.
+    
+    This is different from objects that grant inherent permissions through a
+    different membership relationship (e.g. Users in a UserGroup inherit perms)
+    """
+    
+    def clean(self):
+        print 'a'
+        
+        data = self.cleaned_data
+        if 'user' in data:
+            user = data['user']
+            perms = data['permissions']
+            
+            # if user does not have permissions, then this is a new user:
+            #    - permissions must be selected
+            if not user.get_perms(self.object) and not perms:
+                msg = "You must grant at least 1 permission for new users"
+                self._errors["permissions"] = self.error_class([msg])
+        else:
+            msg = "User is required"
+            self._errors["user"] = self.error_class([msg])
+        
+        return data
