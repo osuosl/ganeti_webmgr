e0fcdce3e1c9258225bd7e96678942baf9c2a925
Peter Krenesky <peter@osuosl.org>
2010-10-05 12:38:34 -0700

    fixing url mappings to use format strings

--- ganeti/urls.py
+++ ganeti/urls.py
@@ -19,29 +19,25 @@ urlpatterns += patterns('ganeti_webmgr.ganeti.views.cluster',
     #   List
     url(r'^clusters/$', 'list', name="cluster-list"),
     #   Detail
-    url(r'^' + cluster + '/?$', 'detail', name="cluster-detail"),
+    url(r'^%s/?$' % cluster, 'detail', name="cluster-detail"),
     url(r'^%s/users/?$' % cluster, 'cluster_users', name="cluster-users"),
     url(r'^%s/user/?$' % cluster, 'permissions', name="cluster-permissions"),
     url(r'^%s/user/quota/?$'% cluster, 'quota', name="cluster-quota"),
 )
 
 # Instances
+prefix = '%s%s' %  (cluster, instance)
 urlpatterns += patterns('ganeti_webmgr.ganeti.views.instances',
     #  List
     url(r'^vms/$', 'list', name="virtualmachine-list"),
     #  Detail
-    url(r'^' + cluster + instance, 'detail', name="instance-detail"),
+    url(r'^%s' % prefix, 'detail', name="instance-detail"),
     #  Create
     url(r'^create/$', 'create', name="instance-create"),
-    url(r'^create/(?P<cluster_slug>\w+)$', 
-        'create', name="instance-create"),
+    url(r'^create/(?P<cluster_slug>\w+)$', 'create', name="instance-create"),
     #  Start, Stop, Reboot
-    url(r'^' + cluster + instance + '/vnc/?$',
-        'vnc', name="instance-vnc"),
-    url(r'^' + cluster + instance + '/shutdown/?$',
-        'shutdown', name="instance-shutdown"),
-    url(r'^' + cluster + instance + '/startup/?$',
-        'startup', name="instance-startup"),
-    url(r'^' + cluster + instance + '/reboot/?$',
-        'reboot', name="instance-reboot"),
+    url(r'^%s/vnc/?$' % prefix, 'vnc', name="instance-vnc"),
+    url(r'^%s/shutdown/?$' % prefix, 'shutdown', name="instance-shutdown"),
+    url(r'^%s/startup/?$' % prefix, 'startup', name="instance-startup"),
+    url(r'^%s/reboot/?$' % prefix, 'reboot', name="instance-reboot"),
 )
