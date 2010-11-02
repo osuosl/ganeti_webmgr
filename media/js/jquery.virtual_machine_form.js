216feece1028f955946d63a04f112dc5742894de
Trevor Bramwell <bramwelt@osuosl.org>
2010-11-01 18:43:45 -0700

    Added second url to cluster_change function. Created function to handle data from second url and select/update form values depending on what came back from the ajax call.

--- media/js/jquery.virtual_machine_form.js
+++ media/js/jquery.virtual_machine_form.js
@@ -59,14 +59,14 @@
         });
     };
     /* Ajax request to update oslist, pnode, and snode when cluster changes */
-    cluster_change = function(url) {
+    cluster_change = function(url1, url2) {
         cluster.change(function() {
             pnode = $("#id_pnode");
             snode = $("#id_snode");
             oslist = $("#id_os");
             id = $(this).children("option:selected").val();
             if( id != '' ) {
-                $.getJSON(url, {'cluster_id':id},
+                $.getJSON(url1, {'cluster_id':id},
                     function(data) {
                         pnode.children().not(':first').remove();
                         snode.children().not(':first').remove();
@@ -85,6 +85,63 @@
                             });
                         });
                     });
+                $.getJSON(url2, {'cluster_id':id},
+                        function(data) {
+                            bootorder = data['bootorder'];
+                            iallocator = data['iallocator'];
+                            hypervisors = data['hypervisors'];
+                            vcpus = data['vcpus'];
+                            rootpath = data['rootpath'];
+                            ram = data['ram'];
+                            kernelpath = data['kernelpath'];
+                            nicmode = data['nicmode'];
+                            nictype = data['nictype'];
+                            imagepath = data['imagepath'];
+                            if(bootorder) {
+                                $("#id_bootorder :selected").removeAttr('selected');
+                                $("#id_bootorder [value="+bootorder+"]").attr('selected','selected');
+                            }
+                            if(hypervisors) {
+                                //list - do nothing for now.
+                            }
+                            if(iallocator) {
+                                hiddendiv = $("<div style='display: none;'>");
+                                hidden = $("<input type='hidden' />");
+                                hidden.attr('name', 'iallocator_hostname');
+                                hidden.attr('value', iallocator);
+                                hiddendiv.append(hidden);
+                                hiddendiv.append("</div>");
+                                $("#id_iallocator").after(hiddendiv);
+                                // check iallocator checkbox
+                                $("#id_iallocator").attr('checked', true);
+                            }
+                            if(kernelpath) {
+                                $("#id_kernelpath").val(kernelpath);
+                            }
+                            if(nicmode) {
+                                $("#id_nicmode :selected").removeAttr('selected');
+                                $("#id_nicmode [value="+nicmode+"]").attr('selected','selected');
+                            }
+                            if(nictype) {
+                                $("#id_nictype :selected").removeAttr('selected');
+                                $("#id_nictype [value="+nictype+"]").attr('selected','selected');
+                            }
+                            if(ram) {
+                                $("#id_ram").val(ram);
+                            }
+                            if(rootpath) {
+                                $("#id_rootpath").val(rootpath);
+                            }
+                            if(data['serialconsole']) {
+                                $("#id_serialconsole").attr('checked', true);
+                            }
+                            if(vcpus) {
+                                $("#id_vcpus").val(vcpus);
+                            }
+                            if(imagepath) {
+                                $("#id_imagepath").val(imagepath);
+                            }
+                        });
             }
         });
     };
