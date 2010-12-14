1467941c53a236099d0b0cd55ac8b78bce2ba021
Peter Krenesky <peter@osuosl.org>
2010-12-14 10:54:24 -0800

    improvements for cache_updater:     * once again creates missing VMs     * now properly handles case where mtime is None, previously caused an exception     * fixed a unittest that was still instantiating the full object instead of just checking raw data

--- ganeti/tests/cache_updater.py
+++ ganeti/tests/cache_updater.py
@@ -66,9 +66,6 @@ class TestCacheUpdater(TestCase, VirtualMachineTestCaseMixin):
         # run updater and refresh the objects from the db
         with MuteStdout():
             update_cache()
-        vm0 = VirtualMachine.objects.get(pk=vm0.id)
-        vm1 = VirtualMachine.objects.get(pk=vm1.id)
-        
         vm0 = VirtualMachine.objects.filter(pk=vm0.id).values('mtime','cached','status','operating_system')[0]
         vm1 = VirtualMachine.objects.filter(pk=vm1.id).values('mtime','cached','status','operating_system')[0]
         
@@ -121,6 +118,91 @@ class TestCacheUpdater(TestCase, VirtualMachineTestCaseMixin):
         self.assertEqual("running", vm0['status'])
         self.assertEqual("running", vm1['status'])
         
+        
+        if cached > datetime.fromtimestamp(float(vm0['cached'])):
+            self.fail('cache is not newer: %s, %s' % (cached, vm0.cached))
+        
+        if cached > datetime.fromtimestamp(float(vm1['cached'])):
+            self.fail('cache is not newer: %s, %s' % (cached, vm1.cached))
+    
+    def test_null_timestamps(self):
+        """
+        Tests that the cache updater can handle updating records with null
+        timestamps
+        """
+        vm0, cluster = self.create_virtual_machine()
+        vm1, chaff = self.create_virtual_machine(cluster, 'vm2.osuosl.bak')
+        
+        os = 'image+gentoo-hardened-cf'
+        mtime_timestamp = 1285883000.8692000
+        mtime = datetime.fromtimestamp(mtime_timestamp)
+        cached = datetime.now()
+        
+        # set data so that mtime is up to date.  include a data change that
+        # can be test for
+        data = list(INSTANCES_BULK)
+        data[0]['mtime'] = mtime_timestamp
+        data[1]['mtime'] = mtime_timestamp
+        cluster.rapi.GetInstances.response = data
+        VirtualMachine.objects.all().update(mtime=None, cached=None, \
+                                operating_system='image+fake', status='running')
+        
+        # run updater and refresh the objects from the db
+        with MuteStdout():
+            update_cache()
+        vm0 = VirtualMachine.objects.filter(pk=vm0.id).values('mtime','cached','status','operating_system')[0]
+        vm1 = VirtualMachine.objects.filter(pk=vm1.id).values('mtime','cached','status','operating_system')[0]
+        
+        # properties should not be updated because mtime and status indicated it
+        # was already up to date
+        self.assertEqual(os, vm0['operating_system'])
+        self.assertEqual(os, vm1['operating_system'])
+        self.assertEqual(mtime_timestamp, float(vm0['mtime']))
+        self.assertEqual(mtime_timestamp, float(vm1['mtime']))
+        self.assertEqual("running", vm0['status'])
+        self.assertEqual("running", vm1['status'])
+        
+        if cached > datetime.fromtimestamp(float(vm0['cached'])):
+            self.fail('cache is not newer: %s, %s' % (cached, vm0.cached))
+        
+        if cached > datetime.fromtimestamp(float(vm1['cached'])):
+            self.fail('cache is not newer: %s, %s' % (cached, vm1.cached))
+    
+    def test_missing_vms(self):
+        """
+        Tests the cache updater importing missing vms
+        """
+        vm0, cluster = self.create_virtual_machine()
+        
+        os = 'image+gentoo-hardened-cf'
+        mtime_timestamp = 1285883000.8692000
+        mtime = datetime.fromtimestamp(mtime_timestamp)
+        cached = datetime.now()
+        
+        # set data so that mtime is up to date.  include a data change that
+        # can be test for
+        data = list(INSTANCES_BULK)
+        data[0]['mtime'] = mtime_timestamp
+        data[1]['mtime'] = mtime_timestamp
+        cluster.rapi.GetInstances.response = data
+        VirtualMachine.objects.all().update(mtime=None, cached=None, \
+                                operating_system='image+fake', status='running')
+        
+        # run updater and refresh the objects from the db
+        with MuteStdout():
+            update_cache()
+        self.assert_(VirtualMachine.objects.filter(hostname='vm2.osuosl.bak').exists())
+        vm0 = VirtualMachine.objects.filter(pk=vm0.id).values('mtime','cached','status','operating_system')[0]
+        vm1 = VirtualMachine.objects.filter(hostname='vm2.osuosl.bak').values('mtime','cached','status','operating_system')[0]
+        
+        # properties should not be updated because mtime and status indicated it
+        # was already up to date
+        self.assertEqual(os, vm0['operating_system'])
+        self.assertEqual(os, vm1['operating_system'])
+        self.assertEqual(mtime_timestamp, float(vm0['mtime']))
+        self.assertEqual(mtime_timestamp, float(vm1['mtime']))
+        self.assertEqual("running", vm0['status'])
+        self.assertEqual("running", vm1['status'])
         
         if cached > datetime.fromtimestamp(float(vm0['cached'])):
             self.fail('cache is not newer: %s, %s' % (cached, vm0.cached))
