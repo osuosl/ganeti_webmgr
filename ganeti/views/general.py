c343c5692ebc3dec71a8e4a551aa43b36517a153
Corbin Simpson <simpsoco@osuosl.org>
2011-01-11 23:28:47 -0800

    views: Remove unused imports in general.

--- ganeti/views/general.py
+++ ganeti/views/general.py
@@ -17,9 +17,9 @@
 # USA.
 
 
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.contenttypes.models import ContentType
