1d1f1bb192d567a635db99654573f0e1d2b52a24
Corbin Simpson <simpsoco@osuosl.org>
2011-02-03 17:06:29 -0800

    admin: Fix imports.

--- ganeti/admin.py
+++ ganeti/admin.py
@@ -18,7 +18,8 @@
 
 
 from django.contrib import admin
-from models import *
+from ganeti.models import Cluster, VirtualMachine, Profile, Organization, \
+    Quota
 
 
 class QuotaInline(admin.TabularInline):
