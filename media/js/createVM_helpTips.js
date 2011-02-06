40b842616de7224c52002b304fb177cf2d2f0722
Rob McGuire-Dale <rob@osuosl.org>
2011-02-05 22:11:33 -0800

    Ticket 3231: Moving help tips to its own CSS file.

---
+++ media/js/createVM_helpTips.js
@@ -0,0 +1,22 @@
+function initHelpTips(){
+    /* initialize the help tips for each item */
+    
+    $('#virtualmachineform input, #virtualmachineform select')
+            .live('focus', function(){
+            
+            name = this.name;
+            label = $(this).prev('label').html()
+            $content = $('#help-'+name);
+            
+            if($content.length != 0){
+                $('#help')
+                    .show();
+                $('#help div')
+                    .empty()
+                    .html($content.html());
+                $('#help h3')
+                    .empty()
+                    .html(label);
+            }
+        });
+}
