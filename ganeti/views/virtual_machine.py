20a5d1bcb6cd7e55133ec77e3a8e2df730007ed2
Peter Krenesky <peter@osuosl.org>
2011-02-23 10:57:42 -0800

    Ticket #3465 - clearing the job error from a failed create results in vm that cannot be recovered by a user

--- ganeti/views/virtual_machine.py
+++ ganeti/views/virtual_machine.py
@@ -17,6 +17,7 @@
 # USA.
 
 import json
+from django.contrib.contenttypes.models import ContentType
 
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -438,18 +439,7 @@ def detail(request, cluster_slug, instance):
     if not (admin or power or remove or modify or tags):
         return render_403(request, 'You do not have permission to view this cluster\'s details')
 
-    # check job for pending jobs that should be rendered with a different
-    # detail template.  This allows us to reduce the chance that users will do
-    # something strange like rebooting a VM that is being deleted or is not
-    # fully created yet.
-    if vm.pending_delete:
-        template = 'virtual_machine/delete_status.html'
-    elif vm.template or vm.info is None:
-        template = 'virtual_machine/create_status.html'
-    else:
-        template = 'virtual_machine/detail.html'
-    
-    return render_to_response(template, {
+    context = {
         'cluster': cluster,
         'instance': vm,
         'admin':admin,
@@ -458,7 +448,26 @@ def detail(request, cluster_slug, instance):
         'power':power,
         'modify':modify,
         'migrate':migrate,
-        },
+    }
+
+    # check job for pending jobs that should be rendered with a different
+    # detail template.  This allows us to reduce the chance that users will do
+    # something strange like rebooting a VM that is being deleted or is not
+    # fully created yet.
+    if vm.pending_delete:
+        template = 'virtual_machine/delete_status.html'
+    elif vm.template or vm.info is None:
+        template = 'virtual_machine/create_status.html'
+        if vm.last_job:
+            context['job'] = vm.last_job
+        else:
+            ct = ContentType.objects.get_for_model(vm)
+            context['job'] = Job.objects.order_by('-finished') \
+                .filter(content_type=ct, object_id=vm.pk)[0]
+    else:
+        template = 'virtual_machine/detail.html'
+    
+    return render_to_response(template, context,
         context_instance=RequestContext(request),
     )
 
