06f0f6cb93aee49bd57a79b2139560e6600e8d78
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2011-04-27 19:34:46 -0700

    Ticket #2739: Experimenting with auto-complete search queries for partial-term searching.

--- ganeti/search_indexes.py
+++ ganeti/search_indexes.py
@@ -1,13 +1,16 @@
-from haystack.indexes import *
+from haystack import indexes
 from haystack import site
 from ganeti.models import VirtualMachine
 
-class VirtualMachineIndex(RealTimeSearchIndex):
-    text = CharField(document=True, use_template=True)
+class VirtualMachineIndex(indexes.RealTimeSearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
 
     # We can pull data strait out of the model via `model_attr`
-    hostname = CharField(model_attr='hostname')
+    hostname = indexes.CharField(model_attr='hostname')
     
+    # Experimenting with auto-complete
+    content_auto = indexes.EdgeNgramField(model_attr='hostname')
+
     def get_queryset(self):
         return VirtualMachine.objects.all()
 
