892d3137a82d614a80ce0f8bb72d777014b71187
Rob McGuire-Dale <rob.mcguiredale+git@gmail.com>
2011-05-20 21:16:41 -0700

    Ticket #2739: Adding search index for Clusters.

--- ganeti/search_indexes.py
+++ ganeti/search_indexes.py
@@ -1,17 +1,40 @@
 from haystack.indexes import *
 from haystack import site
-from ganeti.models import VirtualMachine
+from ganeti.models import VirtualMachine, Cluster
+
+''' Haystack search indexex.
+
+This is where the indexes are defined. They fill the Haystack search query set
+(the set of objects that are searchable.) There should be one index defined per
+GWM object.
+'''
 
 class VirtualMachineIndex(RealTimeSearchIndex):
+    ''' Search index for VirtualMachines '''
+
     text = CharField(document=True, use_template=True)
 
     # We can pull data strait out of the model via `model_attr`
-    hostname = CharField(model_attr='hostname')
+    # (Commmenting out 'cause I'm not sure it's needed)
+    # hostname = CharField(model_attr='hostname')
 
-    # Experimenting with auto-complete
+    # Autocomplete search field on the `hostname` model field
     content_auto = EdgeNgramField(model_attr='hostname')
 
     def get_queryset(self):
         return VirtualMachine.objects.all()
 
 site.register(VirtualMachine, VirtualMachineIndex)
+
+class ClusterIndex(RealTimeSearchIndex):
+    ''' Search index for Clusters '''
+
+    text = CharField(document=True, use_template=True)
+
+    # Autocomplete search field on `hostname` model field
+    content_auto = EdgeNgramField(model_attr='hostname')
+
+    def get_queryset(self):
+        return Cluster.objects.all()
+
+site.register(Cluster, ClusterIndex)
