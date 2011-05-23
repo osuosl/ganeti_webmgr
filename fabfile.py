ccbf2a92faf98300b70a0c607b8279f3873b5ded
Peter Krenesky <peter@osuosl.org>
2011-05-23 11:08:59 -0700

    Fabric: adding twisted >11.0.0

--- fabfile.py
+++ fabfile.py
@@ -27,6 +27,7 @@ PIP_INSTALL = {
     'django-haystack'           :'>=1.2.1',
     'whoosh'                    :'>=1.8.1',
     'django_object_permissions' :'=1.3.1',
+    'twisted'                   :'>=11.0.0'
 }
 
 GIT_INSTALL =  {
