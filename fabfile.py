48d0024bf96076eeeebb99e9875373c865d4e762
Peter Krenesky <peter@osuosl.org>
2011-05-23 09:59:04 -0700

    fabric: setting specific rev on object permissions

--- fabfile.py
+++ fabfile.py
@@ -26,7 +26,7 @@ PIP_INSTALL = {
     'pysqlite'                  :'',
     'django-haystack'           :'>=1.2.1',
     'whoosh'                    :'>=1.8.1',
-    'django_object_permissions' :'>=1.3.1',
+    'django_object_permissions' :'=1.3.1',
 }
 
 GIT_INSTALL =  {
