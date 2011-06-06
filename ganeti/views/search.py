58048d9de0b69353bde56023a23921a9e06373fc
Rob McGuire-Dale <rob.mcguiredale+git@gmail.com>
2011-06-05 20:54:40 -0700

    Ticket #6015: Removing any kind of URL lookup from the suggestions view, creating a new detail lookup view.

--- ganeti/views/search.py
+++ ganeti/views/search.py
@@ -1,11 +1,13 @@
 import json
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 from haystack.query import SearchQuerySet
+from ganeti.models import VirtualMachine, Cluster, Node
 
 
 @login_required
-def search_json(request):
+def suggestions(request):
     ''' Return a list of search results for the autocomplete search box.
 
     Return a list of search results for the query in the GET parameter `term` 
@@ -18,22 +20,18 @@ def search_json(request):
             {
                 'value':    'foo',
                 'type':     'vm',
-                'url':      'vm/foo'
             },
             {
                 'value':    'bar',
                 'type':     'vm',
-                'url':      'vm/bar'
             },
             {
                 'value':    'herp',
                 'type':     'cluster',
-                'url':      'cluster/herp'
             },
             {
                 'value':    'derp',
                 'type':     'node',
-                'url':      'node/derp'
             }
         ]
     '''
@@ -61,16 +59,39 @@ def search_json(request):
                 result_object['type'] = 'node'
             else:
                 result_object['type'] = 'unknown'
-
-            if hasattr(result.object, 'get_absolute_url'):
-                result_object['url'] = result.object.get_absolute_url()
             result_objects.append(result_object)
 
     # Return the results list as a json object
     return HttpResponse(json.dumps(result_objects, indent=4), 
             mimetype='application/json')
-    
-def detail_lookup(request):
-    
 
-    return HttpResponse('', mimetype='application/json')
+@login_required
+def detail_lookup(request):
+    object_type = request.GET.get('type', None)
+    hostname = request.GET.get('hostname', None)
+    URL = []
+    if object_type and hostname:
+        try:
+            if object_type == 'vm':
+                vm = VirtualMachine.objects.filter(hostname=hostname)[0]
+                URL.append(reverse('instance-detail', 
+                        args=[
+                            vm.cluster.slug, 
+                            vm.hostname
+                        ]))
+            elif object_type == 'cluster':
+                cluster = Cluster.objects.filter(hostname=hostname)[0]
+                URL.append(reverse('cluster-detail', args=[cluster.slug]))
+            elif object_type == 'node':
+                node = Node.objects.filter(hostname=hostname)[0]
+                URL.append(reverse('node-detail', 
+                        args=[
+                            node.cluster.slug, 
+                            node.hostname
+                        ]))
+
+        # If an object can't be found, just return a blank URL
+        except IndexError:
+            pass 
+
+    return HttpResponse(json.dumps(URL, indent=4), mimetype='application/json')
