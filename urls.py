9dcf49a75430fad4b6b16b4d1609bbd731620cb6
Bojan Suzic <bojan.suzic@gmail.com>
2011-06-18 16:16:06 +0200

    changing to 0.7.x

--- urls.py
+++ urls.py
@@ -29,6 +29,7 @@ urlpatterns = patterns('',
     (r'^', include('object_permissions.urls')),
     (r'^', include('object_log.urls')),
     (r'^', include('muddle_users.urls')),
+    (r'^', include('api.urls')),
 
     # user management
     # account/activate/<key>/ - Activate a user
