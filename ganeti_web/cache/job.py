473f06220537f38461f28e86e0b7b7163df583df
Peter Krenesky <peter@osuosl.org>
2011-07-18 18:29:57 -0700

    GWM_CACHE: starting rewrite of job cache function (#2403)

--- ganeti_web/cache/job.py
+++ ganeti_web/cache/job.py
@@ -24,13 +24,33 @@ from twisted.internet import reactor
 from twisted.internet.defer import DeferredList, Deferred
 from twisted.web import client
 from ganeti_web.cacher import Timer, Counter
-from ganeti_web.models import Cluster, VirtualMachine
+from ganeti_web.models import Cluster, VirtualMachine, Job
 
 
 JOBS_URL = 'https://%s:%s/2/jobs'
 JOB_URL = 'https://%s/%s/2/jobs/%s'
+COMPLETE_STATUS = ('success', 'error')
 
 class JobCacheUpdater(object):
+
+    def update_sync(self):
+        """
+        TODO: make this into an async method.  this method is just a test to see
+        if an efficient update algorithm is possible
+
+        1) query job ids from cluster
+
+        Running Jobs:
+        2) create base query of jobs with those ids
+        3) filter jobs based on those not yet finished
+        4) query running jobs individually, updating the job and related object as needed
+        5) remove id from list
+
+        New Jobs:
+        6) any job leftover in the list is not yet in the database
+        """
+        #for cluster in Cluster.objects.all()
+        pass
 
     def update(self):
         """
@@ -60,32 +80,40 @@ class JobCacheUpdater(object):
         process data received from ganeti.
         """
         print '%s:' % cluster.hostname
-        ids = json.loads(info)
+        ids = set(json.loads(info))
         self.timer.tick('info fetched from ganeti     ')
         updated = Counter()
 
-        # only import new jobs
-        db_ids = set(cluster.jobs.values_list('pk', flat=True))
-        new_ids = (id for id in ids if id not in db_ids)
-        deferreds = [self.update_job(cluster, pk, updated) for pk in new_ids]
-        deferred_list = DeferredList(deferreds)
+        # fetch list of jobs in the cluster that are not yet finished.  if the
+        # job is already finished then we don't need to update it
+        db_ids = cluster.jobs \
+                            .exclude(status__in=COMPLETE_STATUS) \
+                            .values_list('job_id', flat=True)
 
-        # batch update the cache updated time for all VMs in this cluster. This
-        # will set the last updated time for both VMs that were modified and for
-        # those that weren't.  even if it wasn't modified we want the last
-        # updated time to be up to date.
-        #
-        # XXX don't bother checking to see whether this query needs to run.  It
-        # normal usage it will almost always need to
-        # XXX undefined name "infos"
-        def update_timestamps(result):
-            print '    updated: %s out of %s' % (updated, len(infos))
-            self.timer.tick('records or timestamps updated')
-        deferred_list.addCallback(update_timestamps)
+        # update all running jobs
+        # XXX this could be a choke point if there are many running jobs.  each
+        # job will be a separate ganeti query
+        deferreds = [self.update_job(id) for id in db_ids]
+        ids -= set(db_ids)
+        
+        # get list of jobs that are finished.  use this to filter the list of
+        # ids further
+        # XXX this could be a joke point if there are a lot of IDs that are have
+        # completed but have not yet been archived by ganeti.
+        db_ids = cluster.jobs \
+                            .filter(id__in=ids, status__in=COMPLETE_STATUS) \
+                            .values_list('job_id', flat=True)
+        ids -= set(db_ids)
+        
+        # any job id still left in the list is a new job.  Create the job and
+        # associate it with the object it relates to
+        for id in ids:
+            deferreds.append(self.import_job(id))
 
         # XXX it would be nice if the deferred list could be returned and this
         # callback hooked up outside of the method, but that doesn't seem
         # possible
+        deferred_list = DeferredList(deferreds)
         deferred_list.addCallback(callback)
 
     def get_job(self, cluster, id, updated):
