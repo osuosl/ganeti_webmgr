8713406280d96a288155dc4fa3a198da24c75bd2
Peter Krenesky <peter@osuosl.org>
2011-08-05 09:59:42 -0700

    Ticket #7443 - Can't remove keys from profile:    user detail template was missing shots&mixers

--- ganeti_web/muddle/mixers.py
+++ ganeti_web/muddle/mixers.py
@@ -5,5 +5,5 @@ register('main_menu', TemplateMixer('ganeti/muddle/main_menu_search.html'))
 register('admin_menu', TemplateMixer('ganeti/muddle/admin_menu.html'))
 
 register('user-detail-tab', TemplateMixer('ganeti/muddle/user_usage.html'))
+register('user-detail-content', TemplateMixer('ganeti/muddle/user_sshkeys.html'))
 register('gruop-detail-tab', TemplateMixer('ganeti/muddle/group_usage.html'))
-
