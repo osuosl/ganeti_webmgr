513c5267ecf451ccf77e83d9a3e4e7a89a4fbc16
Corbin Simpson <simpsoco@osuosl.org>
2011-12-15 17:12:05 -0800

    tests/accounts: Cleanups and test splitting.

--- ganeti_web/tests/accounts.py
+++ ganeti_web/tests/accounts.py
@@ -24,203 +24,296 @@ from django.test.client import Client
 from ganeti_web.models import Profile
 
 
-global user
-global c
-
-
 __all__ = ('TestProfileModel', 'TestAccountViews',)
 
 
 class TestProfileModel(TestCase):
 
-    def setUp(self):
-        self.tearDown()
-
     def tearDown(self):
         Profile.objects.all().delete()
         User.objects.all().delete()
-    
+
     def test_trivial(self):
         """ Tests that object can be created """
         Profile()
-    
+
     def test_signal_listeners(self):
         """
         Test automatic creation and deletion of profile objects
         """
         user = User(username='tester')
         user.save()
-        
+
         # profile created
         profile = user.get_profile()
         self.assertTrue(profile, 'profile was not created')
-        
+
         # profile deleted
         user.delete()
         self.assertFalse(Profile.objects.filter(id=profile.id).exists())
 
 
 class TestAccountViews(TestCase):
-    
-    def setUp(self):
-        self.tearDown()
 
+    def setUp(self):
         user = User(username='tester', email='test@test.com')
         user.set_password('secret')
         user.save()
-        
-        client = Client()
-        
-        g = globals()
-        g['user'] = user
-        g['c'] = client
-    
+
+        self.c = Client()
+        self.user = user
+
     def tearDown(self):
+        self.user.delete()
+
         Profile.objects.all().delete()
         User.objects.all().delete()
-    
-    def test_view_login(self):
+
+    def test_view_login_anonymous(self):
         """
         Test logging in
         """
         url = '/accounts/login/'
-        
+
         # anonymous user
-        response = c.get(url, follow=True)
+        response = self.c.get(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_no_username(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # no username
         data = {'password':'secret'}
-        response = c.post(url, data)
+        response = self.c.post(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_no_password(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # no password
         data = {'username':'tester'}
-        response = c.post(url, data)
+        response = self.c.post(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_invalid_username(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # bad username
         data = {'username':'invalid', 'password':'secret'}
-        response = c.post(url, data)
+        response = self.c.post(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_invalid_password(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # bad password
         data = {'username':'tester', 'password':'incorrect'}
-        response = c.post(url, data)
+        response = self.c.post(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_no_perms(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # user with perms on no virtual machines
-        self.assertTrue(c.login(username=user.username, password='secret'))
-        response = c.post(url, follow=True)
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        response = self.c.post(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_invalid_method(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # invalid method
         data = {'username':'tester', 'password':'secret'}
-        response = c.get(url, data, follow=True)
+        response = self.c.get(url, data, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_login_success(self):
+        """
+        Test logging in
+        """
+        url = '/accounts/login/'
+
         # success
         data = {'username':'tester', 'password':'secret'}
-        response = c.post(url, data, follow=True)
+        response = self.c.post(url, data, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/overview.html')
-    
-    def test_view_logout(self):
+
+    def test_view_logout_anonymous(self):
         """
         Test logging out
         """
         url = '/accounts/logout/'
-        
+
         # anonymous user
-        response = c.get(url, follow=True)
+        response = self.c.get(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_logout_success(self):
+        """
+        Test logging out
+        """
+        url = '/accounts/logout/'
+
         # successful logout
-        self.assertTrue(c.login(username=user.username, password='secret'))
-        response = c.get(url, follow=True)
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        response = self.c.get(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'registration/login.html')
-    
-    def test_view_profile(self):
+
+    def test_view_profile_anonymous(self):
         """
         Tests updating a user profile
         """
         url = '/accounts/profile/'
-        user = globals()['user']
-        
+
         # anonymous user
-        response = c.get(url, follow=True)
+        response = self.c.get(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
-        
+
+    def test_view_profile_form(self):
+        """
+        Tests updating a user profile
+        """
+        url = '/accounts/profile/'
+
         # get form
-        self.assertTrue(c.login(username=user.username, password='secret'))
-        response = c.get(url)
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/users/profile.html')
-        
+
+    def test_view_profile_invalid_method(self):
+        """
+        Tests updating a user profile
+        """
+        url = '/accounts/profile/'
+
+        # get form
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        self.c.get(url)
+
         # bad method (CSRF check)
         data = {'email':'new@test.com', 'old_password':'secret','new_password':'foo', 'confirm_password':'foo'}
-        response = c.get(url, data)
+        response = self.c.get(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/users/profile.html')
-        user = User.objects.get(id=user.id)
+        user = User.objects.get(id=self.user.id)
         self.assertEqual('test@test.com', user.email)
         self.assertTrue(user.check_password('secret'), 'Password should not have been changed')
-        
+
+    def test_view_profile_invalid_old_password(self):
+        """
+        Tests updating a user profile
+        """
+        url = '/accounts/profile/'
+
+        # get form
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        self.c.get(url)
+
         # bad old password
         data = {'email':'new@test.com', 'old_password':'incorrect','new_password':'foo', 'confirm_password':'foo'}
-        response = c.get(url, data)
+        response = self.c.get(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/users/profile.html')
-        user = User.objects.get(id=user.id)
+        user = User.objects.get(id=self.user.id)
         self.assertEqual('test@test.com', user.email)
         self.assertTrue(user.check_password('secret'), 'Password should not have been changed')
-        
+
+    def test_view_profile_unconfirmed(self):
+        """
+        Tests updating a user profile
+        """
+        url = '/accounts/profile/'
+
+        # get form
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        self.c.get(url)
+
         # not confirmed
         data = {'email':'new@test.com', 'old_password':'secret','new_password':'foo', 'confirm_password':'incorrect'}
-        response = c.get(url, data)
+        response = self.c.get(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/users/profile.html')
-        user = User.objects.get(id=user.id)
+        user = User.objects.get(id=self.user.id)
         self.assertEqual('test@test.com', user.email)
         self.assertTrue(user.check_password('secret'), 'Password should not have been changed')
-        
+
+    def test_view_profile_change_email(self):
+        """
+        Tests updating a user profile
+        """
+        url = '/accounts/profile/'
+
+        # get form
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        self.c.get(url)
+
         # change email
-        response = c.post(url, {'email':'new@test.com'})
+        response = self.c.post(url, {'email':'new@test.com'})
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/users/profile.html')
-        user = User.objects.get(id=user.id)
+        user = User.objects.get(id=self.user.id)
         self.assertEqual('new@test.com', user.email)
         self.assertTrue(user.check_password('secret'), 'Password should not have been changed')
-        
+
+    def test_view_profile_change_password(self):
+        """
+        Tests updating a user profile
+        """
+        url = '/accounts/profile/'
+
+        # get form
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
+        self.c.get(url)
+
         # change password
         data = {'email':'new2@test.com', 'old_password':'secret','new_password':'foo', 'confirm_password':'foo'}
-        response = c.post(url, data)
+        response = self.c.post(url, data)
         self.assertEqual(200, response.status_code)
         self.assertEquals('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/users/profile.html')
-        user = User.objects.get(id=user.id)
+        user = User.objects.get(id=self.user.id)
         self.assertEqual('new2@test.com', user.email)
         self.assertTrue(user.check_password('foo'), 'Password was not been changed')
