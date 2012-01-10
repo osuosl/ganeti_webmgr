4ab115f1c6e3a211bde5078d292b2d16a18b3f5c
Corbin Simpson <simpsoco@osuosl.org>
2012-01-10 15:06:25 -0800

    tests/views/vm/general: Deglobalize.

--- ganeti_web/tests/views/virtual_machine/general.py
+++ ganeti_web/tests/views/virtual_machine/general.py
@@ -10,13 +10,8 @@ __all__ = ['TestVirtualMachineViewList',
            'TestVirtualMachineDetailView',
            'TestVirtualMachineSSHKeysView']
 
-global c, cluster, vm
-global user, user1, superuser, vm_admin, cluster_admin
-
 
 class TestVirtualMachineViewList(TestVirtualMachineViewsBase):
-
-    context = globals()
 
     def test_anonymous(self):
         """
@@ -25,7 +20,7 @@ class TestVirtualMachineViewList(TestVirtualMachineViewsBase):
         """
         url = '/vms/'
 
-        response = c.get(url, follow=True)
+        response = self.c.get(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
 
@@ -37,10 +32,11 @@ class TestVirtualMachineViewList(TestVirtualMachineViewsBase):
 
         url = '/vms/'
 
-        self.create_virtual_machine(cluster, 'test1')
+        self.create_virtual_machine(self.cluster, 'test1')
 
-        self.assertTrue(c.login(username=user.username, password='secret'))
-        response = c.get(url)
+        self.assertTrue(self.c.login(username=self.user.username,
+                                     password='secret'))
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         self.assertEqual('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/virtual_machine/list.html')
@@ -56,19 +52,20 @@ class TestVirtualMachineViewList(TestVirtualMachineViewsBase):
         url = '/vms/'
 
         # setup vms and perms
-        vm1, cluster1 = self.create_virtual_machine(cluster, 'test1')
-        self.create_virtual_machine(cluster, 'test2')
-        user1.grant('admin', vm)
-        user1.grant('admin', vm1)
+        vm1, cluster1 = self.create_virtual_machine(self.cluster, 'test1')
+        self.create_virtual_machine(self.cluster, 'test2')
+        self.user1.grant('admin', self.vm)
+        self.user1.grant('admin', vm1)
 
         # user with some perms
-        self.assertTrue(c.login(username=user1.username, password='secret'))
-        response = c.get(url)
+        self.assertTrue(self.c.login(username=self.user1.username,
+                                     password='secret'))
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         self.assertEqual('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/virtual_machine/list.html')
         vms = response.context['vms'].object_list
-        self.assertEqual(set(vms), set([vm, vm1]))
+        self.assertEqual(set(vms), set([self.vm, vm1]))
 
     def test_superuser(self):
         """
@@ -82,38 +79,36 @@ class TestVirtualMachineViewList(TestVirtualMachineViewsBase):
         user2.save()
 
         # setup vms and perms
-        vm1, cluster1 = self.create_virtual_machine(cluster, 'test1')
-        vm2, cluster1 = self.create_virtual_machine(cluster, 'test2')
-        vm3, cluster1 = self.create_virtual_machine(cluster, 'test3')
+        vm1, cluster1 = self.create_virtual_machine(self.cluster, 'test1')
+        vm2, cluster1 = self.create_virtual_machine(self.cluster, 'test2')
+        vm3, cluster1 = self.create_virtual_machine(self.cluster, 'test3')
 
         # authorized (superuser)
-        self.assertTrue(c.login(username=user2.username, password='secret'))
-        response = c.get(url)
+        self.assertTrue(self.c.login(username=user2.username, password='secret'))
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         self.assertEqual('text/html; charset=utf-8', response['content-type'])
         self.assertTemplateUsed(response, 'ganeti/virtual_machine/list.html')
         vms = response.context['vms'].object_list
-        self.assertEqual(set(vms), set([vm, vm1, vm2, vm3]))
+        self.assertEqual(set(vms), set([self.vm, vm1, vm2, vm3]))
 
 
 class TestVirtualMachineDetailView(TestVirtualMachineViewsBase):
-
-    context = globals()
 
     def test_view_detail(self):
         """
         Test showing virtual machine details
         """
         url = '/cluster/%s/%s/'
-        args = (cluster.slug, vm.hostname)
+        args = (self.cluster.slug, self.vm.hostname)
 
         self.assert_standard_fails(url, args)
-        self.assert_200(url, args, [superuser, vm_admin, cluster_admin], template='ganeti/virtual_machine/detail.html')
+        self.assert_200(url, args, [self.superuser, self.vm_admin,
+                                    self.cluster_admin],
+                        template='ganeti/virtual_machine/detail.html')
 
 
 class TestVirtualMachineSSHKeysView(TestVirtualMachineViewsBase):
-
-    context = globals()
 
     def test_view_ssh_keys(self):
         """
@@ -121,15 +116,15 @@ class TestVirtualMachineSSHKeysView(TestVirtualMachineViewsBase):
         specified virtual machine
         """
         # second virtual machine created
-        vm1, cluster1 = self.create_virtual_machine(cluster, 'vm2.osuosl.bak')
+        vm1, cluster1 = self.create_virtual_machine(self.cluster, 'vm2.osuosl.bak')
 
         # grant admin permission to first user
-        user.grant("admin", vm)
+        self.user.grant("admin", self.vm)
 
         # add some keys
-        key = SSHKey(key="ssh-rsa test test@test", user=user)
+        key = SSHKey(key="ssh-rsa test test@test", user=self.user)
         key.save()
-        key1 = SSHKey(key="ssh-dsa test asd@asd", user=user)
+        key1 = SSHKey(key="ssh-dsa test asd@asd", user=self.user)
         key1.save()
 
         # get API key
@@ -137,27 +132,37 @@ class TestVirtualMachineSSHKeysView(TestVirtualMachineViewsBase):
         key = settings.WEB_MGR_API_KEY
 
         # forbidden
-        response = c.get( reverse("instance-keys", args=[cluster.slug, vm.hostname, key+"a"]))
-        self.assertEqual( 403, response.status_code )
+        response = self.c.get(reverse("instance-keys",
+                                      args=[self.cluster.slug,
+                                            self.vm.hostname, key+"a"]))
+        self.assertEqual(403, response.status_code)
 
         # not found
-        response = c.get( reverse("instance-keys", args=[cluster.slug, vm.hostname+"a", key]))
-        self.assertEqual( 404, response.status_code )
-        response = c.get( reverse("instance-keys", args=[cluster.slug+"a", vm.hostname, key]))
-        self.assertEqual( 404, response.status_code )
+        response = self.c.get(reverse("instance-keys",
+                                      args=[self.cluster.slug,
+                                            self.vm.hostname+"a", key]))
+        self.assertEqual(404, response.status_code)
+        response = self.c.get(reverse("instance-keys",
+                                      args=[self.cluster.slug+"a",
+                                            self.vm.hostname, key]))
+        self.assertEqual(404, response.status_code)
 
         # vm with users who have admin perms
-        response = c.get( reverse("instance-keys", args=[cluster.slug, vm.hostname, key]))
-        self.assertEqual( 200, response.status_code )
+        response = self.c.get(reverse("instance-keys",
+                                      args=[self.cluster.slug,
+                                            self.vm.hostname, key]))
+        self.assertEqual(200, response.status_code)
         self.assertEquals("application/json", response["content-type"])
-        self.assertEqual( len(json.loads(response.content)), 2 )
+        self.assertEqual(len(json.loads(response.content)), 2)
         self.assertContains(response, "test@test", count=1)
         self.assertContains(response, "asd@asd", count=1)
 
         # vm without users who have admin perms
-        response = c.get( reverse("instance-keys", args=[cluster.slug, vm1.hostname, key]))
-        self.assertEqual( 200, response.status_code )
+        response = self.c.get(reverse("instance-keys",
+                                      args=[self.cluster.slug, vm1.hostname,
+                                            key]))
+        self.assertEqual(200, response.status_code)
         self.assertEquals("application/json", response["content-type"])
-        self.assertEqual( len(json.loads(response.content)), 0 )
+        self.assertEqual(len(json.loads(response.content)), 0 )
         self.assertNotContains(response, "test@test")
         self.assertNotContains(response, "asd@asd")
