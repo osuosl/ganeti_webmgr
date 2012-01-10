1423d683384a704d86a2615d4b12a1aa5dbf195f
Corbin Simpson <simpsoco@osuosl.org>
2012-01-10 14:55:27 -0800

    tests/views/vm/helpers: Deglobalize and cleanup whitespace.

--- ganeti_web/tests/views/virtual_machine/helpers.py
+++ ganeti_web/tests/views/virtual_machine/helpers.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 
 from object_permissions import grant
 
-from ganeti_web import models, constants
+from ganeti_web import models
 from ganeti_web.tests.rapi_proxy import INFO
 from ganeti_web.tests.views.virtual_machine.base import TestVirtualMachineViewsBase
 from ganeti_web.utilities import os_prettify
@@ -14,13 +14,8 @@ __all__ = ['TestVirtualMachineCreateHelpers', 'TestVirtualMachineHelpers']
 
 Cluster = models.Cluster
 
-global c, cluster
-global user, group
-
 
 class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
-
-    context = globals()
 
     def test_view_cluster_choices(self):
         """
@@ -44,38 +39,40 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
 
         # Set each cluster info and update
         for cluster in (cluster0, cluster1, cluster2, cluster3, cluster4, cluster5):
-            cluster.username = user.username
+            cluster.username = self.user.username
             cluster.password = 'foobar'
             cluster.info = INFO
             cluster.save()
 
-        group.user_set.add(user)
+        self.group.user_set.add(self.user)
         group1 = Group(id=43, name='testing_group2')
         group1.save()
         group1.grant('admin',cluster5)
 
         # anonymous user
-        response = c.get(url, follow=True)
+        response = self.c.get(url, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
 
-        self.assertTrue(c.login(username=user.username, password='secret'))
+        self.assertTrue(self.c.login(username=self.user.username, password='secret'))
 
         # Invalid ClusterUser
-        response = c.get(url, {'clusteruser_id':-1})
+        response = self.c.get(url, {'clusteruser_id':-1})
         self.assertEqual(404, response.status_code)
 
         # create_vm permission through a group
-        group.grant('create_vm', cluster3)
-        response = c.get(url, {'clusteruser_id': group.organization.id})
+        self.group.grant('create_vm', cluster3)
+        response = self.c.get(url, {'clusteruser_id':
+                                    self.group.organization.id})
         self.assertEqual(200, response.status_code)
         clusters = json.loads(response.content)
         self.assertTrue([cluster3.id,'group.create_vm'] in clusters)
         self.assertEqual(1, len(clusters))
 
         # admin permission through a group
-        group.grant('admin', cluster4)
-        response = c.get(url, {'clusteruser_id': group.organization.id})
+        self.group.grant('admin', cluster4)
+        response = self.c.get(url, {'clusteruser_id':
+                                    self.group.organization.id})
         self.assertEqual(200, response.status_code)
         clusters = json.loads(response.content)
         self.assertTrue([cluster3.id,'group.create_vm'] in clusters)
@@ -83,16 +80,16 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
         self.assertEqual(2, len(clusters))
 
         # create_vm permission on the user
-        user.grant('create_vm', cluster0)
-        response = c.get(url)
+        self.user.grant('create_vm', cluster0)
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         clusters = json.loads(response.content)
         self.assertTrue([cluster0.id,'user.create_vm'] in clusters)
         self.assertEqual(1, len(clusters), clusters)
 
         # admin permission on the user
-        user.grant('admin', cluster1)
-        response = c.get(url)
+        self.user.grant('admin', cluster1)
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         clusters = json.loads(response.content)
         self.assertTrue([cluster0.id,'user.create_vm'] in clusters)
@@ -100,9 +97,9 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
         self.assertEqual(2, len(clusters))
 
         # Superusers see everything
-        user.is_superuser = True
-        user.save()
-        response = c.get(url)
+        self.user.is_superuser = True
+        self.user.save()
+        response = self.c.get(url)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         clusters = json.loads(response.content)
@@ -119,61 +116,65 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
         Test retrieving list of options a cluster has for vms
         """
         url = '/vm/add/options/?cluster_id=%s'
-        args = cluster.id
+        args = self.cluster.id
 
         # anonymous user
-        response = c.post(url % args, follow=True)
+        response = self.c.post(url % args, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
 
         # unauthorized user
-        self.assertTrue(c.login(username=user.username, password='secret'))
-        response = c.get(url % args)
+        self.assertTrue(self.c.login(username=self.user.username,
+                                     password='secret'))
+        response = self.c.get(url % args)
         self.assertEqual(403, response.status_code)
 
         # invalid cluster
-        response = c.get(url % "-4")
+        response = self.c.get(url % "-4")
         self.assertEqual(404, response.status_code)
 
         # authorized (create_vm)
-        grant(user, 'create_vm', cluster)
-        response = c.get(url % args)
+        grant(self.user, 'create_vm', self.cluster)
+        response = self.c.get(url % args)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         content = json.loads(response.content)
-        self.assertEqual(set([u'gtest1.osuosl.bak', u'gtest2.osuosl.bak', u'gtest3.osuosl.bak']), set(content['nodes']))
+        self.assertEqual(set([u'gtest1.osuosl.bak', u'gtest2.osuosl.bak',
+                              u'gtest3.osuosl.bak']), set(content['nodes']))
         self.assertEqual(content["os"],
             [[u'Image',
                 [[u'image+debian-osgeo', u'Debian Osgeo'],
                 [u'image+ubuntu-lucid', u'Ubuntu Lucid']]
             ]]
         )
-        user.revoke_all(cluster)
+        self.user.revoke_all(self.cluster)
 
         # authorized (cluster admin)
-        grant(user, 'admin', cluster)
-        response = c.get(url % args)
+        grant(self.user, 'admin', self.cluster)
+        response = self.c.get(url % args)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         content = json.loads(response.content)
 
-        self.assertEqual(set([u'gtest1.osuosl.bak', u'gtest2.osuosl.bak', u'gtest3.osuosl.bak']), set(content['nodes']))
+        self.assertEqual(set([u'gtest1.osuosl.bak', u'gtest2.osuosl.bak',
+                              u'gtest3.osuosl.bak']), set(content['nodes']))
         self.assertEqual(content["os"],
             [[u'Image',
                 [[u'image+debian-osgeo', u'Debian Osgeo'],
                 [u'image+ubuntu-lucid', u'Ubuntu Lucid']]
             ]]
         )
-        user.revoke_all(cluster)
+        self.user.revoke_all(self.cluster)
 
         # authorized (superuser)
-        user.is_superuser = True
-        user.save()
-        response = c.get(url % args)
+        self.user.is_superuser = True
+        self.user.save()
+        response = self.c.get(url % args)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         content = json.loads(response.content)
-        self.assertEqual(set([u'gtest1.osuosl.bak', u'gtest2.osuosl.bak', u'gtest3.osuosl.bak']), set(content['nodes']))
+        self.assertEqual(set([u'gtest1.osuosl.bak', u'gtest2.osuosl.bak',
+                              u'gtest3.osuosl.bak']), set(content['nodes']))
         self.assertEqual(content["os"],
             [[u'Image',
                 [[u'image+debian-osgeo', u'Debian Osgeo'],
@@ -186,7 +187,7 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
         Test retrieval of dict of default parameters set on cluster
         """
         url = '/vm/add/defaults/?cluster_id=%s'
-        args = cluster.id
+        args = self.cluster.id
         """
         expected = dict(
             boot_order='disk',
@@ -214,15 +215,15 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
             hypervisors=[['kvm', 'kvm']],
             migration_downtime=30,
             nic_types=[
-                ['', '---------'], 
-                ['rtl8139', 'rtl8139'], 
-                ['ne2k_isa', 'ne2k_isa'], 
-                ['ne2k_pci', 'ne2k_pci'], 
-                ['paravirtual', 'paravirtual'], 
-                ['i82551', 'i82551'], 
-                ['i82557b', 'i82557b'], 
-                ['i82559er', 'i82559er'], 
-                ['pcnet', 'pcnet'], 
+                ['', '---------'],
+                ['rtl8139', 'rtl8139'],
+                ['ne2k_isa', 'ne2k_isa'],
+                ['ne2k_pci', 'ne2k_pci'],
+                ['paravirtual', 'paravirtual'],
+                ['i82551', 'i82551'],
+                ['i82557b', 'i82557b'],
+                ['i82559er', 'i82559er'],
+                ['pcnet', 'pcnet'],
                 ['e1000', 'e1000']
             ],
             kernel_args='ro',
@@ -232,10 +233,10 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
             disk_types=[
                 ['', '---------'],
                 ['paravirtual', 'paravirtual'],
-                ['ide', 'ide'], 
-                ['scsi', 'scsi'], 
-                ['sd', 'sd'], 
-                ['mtd', 'mtd'], 
+                ['ide', 'ide'],
+                ['scsi', 'scsi'],
+                ['sd', 'sd'],
+                ['mtd', 'mtd'],
                 ['pflash', 'pflash']
             ],
             initrd_path='',
@@ -247,8 +248,8 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
             vnc_tls=False,
             use_localtime=False,
             boot_devices=[
-                ['disk', 'Hard Disk'], 
-                ['cdrom', 'CD-ROM'], 
+                ['disk', 'Hard Disk'],
+                ['cdrom', 'CD-ROM'],
                 ['network', 'Network']
             ],
             security_domain='',
@@ -268,53 +269,52 @@ class TestVirtualMachineCreateHelpers(TestVirtualMachineViewsBase):
         )
 
         #anonymous users
-        response = c.post(url % args, follow=True)
+        response = self.c.post(url % args, follow=True)
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'registration/login.html')
 
         #unauthorized user
-        self.assertTrue(c.login(username=user.username, password='secret'))
-        response = c.get(url % args)
+        self.assertTrue(self.c.login(username=self.user.username,
+                                     password='secret'))
+        response = self.c.get(url % args)
         self.assertEqual(403, response.status_code)
 
         #invalid cluster
-        response = c.get(url % "-2")
+        response = self.c.get(url % "-2")
         self.assertEqual(404, response.status_code)
 
         #authorized (create_vm)
-        grant(user, 'create_vm', cluster)
-        response = c.get(url % args)
+        grant(self.user, 'create_vm', self.cluster)
+        response = self.c.get(url % args)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         content = json.loads(response.content)
         self.assertEqual(expected, content, msg=content)
-        user.revoke_all(cluster)
+        self.user.revoke_all(self.cluster)
 
         #authorized (admin)
-        grant(user, 'admin', cluster)
-        response = c.get(url % args)
+        grant(self.user, 'admin', self.cluster)
+        response = self.c.get(url % args)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         content = json.loads(response.content)
         self.assertEqual(expected, content)
-        user.revoke_all(cluster)
+        self.user.revoke_all(self.cluster)
 
         #authorized (superuser)
-        user.is_superuser = True
-        user.save()
-        response = c.get(url % args)
+        self.user.is_superuser = True
+        self.user.save()
+        response = self.c.get(url % args)
         self.assertEqual(200, response.status_code)
         self.assertEqual('application/json', response['content-type'])
         content = json.loads(response.content)
         self.assertEqual(expected, content)
-        user.is_superuser = False
-        user.save()
+        self.user.is_superuser = False
+        self.user.save()
 
 
 class TestVirtualMachineHelpers(TestCase):
 
-    context = globals()
-    
     def test_os_prettify(self):
         """
         Test the os_prettify() helper function.
