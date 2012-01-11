42731f28a64b9a8039808a3684e44bb49392b6b4
Corbin Simpson <simpsoco@osuosl.org>
2012-01-11 12:25:16 -0800

    tests/views/vm/base: Remove global magic setup.

--- ganeti_web/tests/views/virtual_machine/base.py
+++ ganeti_web/tests/views/virtual_machine/base.py
@@ -16,14 +16,12 @@ Cluster = models.Cluster
 Node = models.Node
 Job = models.Job
 
-global c
-global cluster, vm
-global user, users, group, vm_admin, vm_modify, cluster_migrate, \
-    cluster_admin, superuser
-
 class VirtualMachineTestCaseMixin():
     def create_virtual_machine(self, cluster=None, hostname='vm1.osuosl.bak'):
-        cluster = cluster if cluster else Cluster(hostname='test.osuosl.bak', slug='OSL_TEST', username='foo', password='bar')
+        cluster = cluster if cluster else Cluster(hostname='test.osuosl.bak',
+                                                  slug='OSL_TEST',
+                                                  username='foo',
+                                                  password='bar')
         cluster.save()
         cluster.sync_nodes()
         vm = VirtualMachine(cluster=cluster, hostname=hostname)
@@ -31,20 +29,17 @@ class VirtualMachineTestCaseMixin():
         return vm, cluster
 
 
-class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTestMixin, UserTestMixin):
+class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin,
+                                  ViewTestMixin, UserTestMixin):
     """
     Tests for views showing virtual machines
     """
-    context=globals()
 
     def setUp(self):
         models.client.GanetiRapiClient = RapiProxy
-        vm, cluster = self.create_virtual_machine()
-        self.vm = vm
-        self.cluster = cluster
+        self.vm, self.cluster = self.create_virtual_machine()
 
-        context = {}
-        self.create_standard_users(context)
+        self.create_standard_users()
         self.create_users([
               ('user',{'id':69}),
               ('user1',{'id':88}),
@@ -52,33 +47,21 @@ class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTes
               ('vm_modify',{'id':75}),
               ('cluster_migrate',{'id':78}),
               ('cluster_admin',{'id':99}),
-        ], context)
-        globals().update(context)
+        ])
 
-        vm_admin.grant('admin', vm)
-        vm_modify.grant('modify', vm)
-        cluster_migrate.grant('migrate', cluster)
-        cluster_admin.grant('admin', cluster)
+        self.vm_admin.grant('admin', self.vm)
+        self.vm_modify.grant('modify', self.vm)
+        self.cluster_migrate.grant('migrate', self.cluster)
+        self.cluster_admin.grant('admin', self.cluster)
 
-        group = Group(id=42, name='testing_group')
-        group.save()
+        self.group = Group(id=42, name='testing_group')
+        self.group.save()
 
-        # XXX ensure namespaces for this module and child classes are updated
-        context['c'] = Client()
-        context['group'] = group
-        context['vm'] = vm
-        context['cluster'] = cluster
-        context['cluster_admin'] = cluster_admin
-        context['vm_admin'] = vm_admin
-        globals().update(context)
-        self.context.update(context)
-
-        self.group = group
-        self.c = c
+        self.c = Client()
 
     def tearDown(self):
-        if vm is not None:
-            vm.rapi.error = None
+        if self.vm is not None:
+            self.vm.rapi.error = None
 
         VirtualMachineTemplate.objects.all().delete()
         Job.objects.all().delete()
@@ -90,9 +73,11 @@ class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTes
 
     def validate_get(self, url, args, template):
         self.assert_standard_fails(url, args)
-        self.assert_200(url, args, [superuser, vm_admin], template=template)
+        self.assert_200(url, args, [self.superuser, self.vm_admin],
+                        template=template)
 
-    def validate_get_configurable(self, url, args, template=None, mimetype=False, perms=None):
+    def validate_get_configurable(self, url, args, template=None,
+                                  mimetype=False, perms=None):
         """
         More configurable version of validate_get.
         Additional arguments (only if set) affects only authorized user test.
@@ -109,8 +94,9 @@ class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTes
 
         # authorized user (perm)
         if perms:
-            user.set_perms(perms, vm)
-        self.assert_200(url, args, [superuser, user], mime=mimetype, template=template)
+            self.user.set_perms(perms, self.vm)
+        self.assert_200(url, args, [self.superuser, self.user], mime=mimetype,
+                        template=template)
 
     def validate_post_only_url(self, url, args=None, data=dict(), users=None,
                                get_allowed=False):
@@ -122,9 +108,9 @@ class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTes
         is not set, the GET method is checked to make sure it fails.
         """
 
-        vm = globals()['vm']
-        args = args if args else (cluster.slug, vm.hostname)
-        users = users if users else [superuser, vm_admin, cluster_admin]
+        args = args if args else (self.cluster.slug, self.vm.hostname)
+        users = users if users else [self.superuser, self.vm_admin,
+                                     self.cluster_admin]
 
         # Only do the standard assertions if GET is allowed. POST-only URLs
         # generally don't give a shit about any GET requests and will 405 all
@@ -132,8 +118,9 @@ class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTes
         if get_allowed:
             self.assert_standard_fails(url, args)
         else:
-            self.assertTrue(c.login(username=superuser.username, password='secret'))
-            response = c.get(url % args, data)
+            self.assertTrue(self.c.login(username=self.superuser.username,
+                                         password='secret'))
+            response = self.c.get(url % args, data)
             self.assertEqual(405, response.status_code)
 
         def test_json(user, response):
@@ -146,13 +133,13 @@ class TestVirtualMachineViewsBase(TestCase, VirtualMachineTestCaseMixin, ViewTes
             content = json.loads(response.content)
             text = content['__all__'][0]
             self.assertEqual(msg, text)
-            vm.rapi.error = None
+            self.vm.rapi.error = None
 
         self.assert_200(url, args, users, data=data, tests=test_json,
                         mime='application/json', method='post')
 
         msg = "SIMULATING_AN_ERROR"
-        vm.rapi.error = client.GanetiApiError(msg)
-        self.assert_200(url, args, [superuser], data=data,
+        self.vm.rapi.error = client.GanetiApiError(msg)
+        self.assert_200(url, args, [self.superuser], data=data,
                         tests=test_json_error, mime='application/json',
                         method='post')
