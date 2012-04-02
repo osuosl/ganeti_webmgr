f1de352de0b8a4c4e021ad30c9d9c65a61a8db65
Corbin Simpson <simpsoco@osuosl.org>
2012-04-02 14:03:52 -0700

    urls: Use new django-registration URLConf.
    Refs #10023

--- urls.py
+++ urls.py
@@ -32,24 +32,14 @@ urlpatterns = patterns('',
     (r'^', include('muddle_users.urls')),
     (r'^', include('muddle.urls')),
 
-    # user management
-    # account/activate/<key>/ - Activate a user
-    # account/activate/complete/ - Ater-activation page
-    # account/register/ - User registration form
-    # account/register/complete/ - After-registration page
-    # account/register/closed/ - No registration allowed page
-    # ---
-    # account/login - login page
-    # account/logout - logout page
-    # account/password/reset/ - send password reset email
-    # account/password/change/ - change current user password
-
     # Authentication
     url(r'^accounts/login/?', 'django.contrib.auth.views.login',
         name="login",),
     url(r'^accounts/logout/?', 'django.contrib.auth.views.logout',
         {'next_page':'/'}, name="logout"),
-    (r'^accounts/', include('registration.urls')),
+
+    # Registration
+    (r'^accounts/', include('registration.backends.default.urls')),
 
     # Explicit 500 test route
     (r'^500/$', 'django.views.generic.simple.direct_to_template',
