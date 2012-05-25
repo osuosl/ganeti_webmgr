d6a5c04f9f1324e6d8e44f8caae9df2e749ef34e
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-05-25 13:08:50 -0700

    tests/importing_nodes: Fix syntax error.
    Refs #10167.

--- ganeti_web/tests/importing_nodes.py
+++ ganeti_web/tests/importing_nodes.py
@@ -63,7 +63,7 @@ class NodeImportBase(TestCase):
         self.cluster0.rapi.GetNodes.response = ['node0','node2']
         self.cluster1.rapi.GetNodes.response = ['node3','node5']
 
-        self.vm = VirtualMachine.objects.createhostname='gimager.example.bak', cluster=self.cluster0)
+        self.vm = VirtualMachine.objects.create(hostname='gimager.example.bak', cluster=self.cluster0)
 
         self.node0 = Node.objects.create(hostname='node0', cluster=self.cluster0)
         self.node1 = Node.objects.create(hostname='node1', cluster=self.cluster0)
