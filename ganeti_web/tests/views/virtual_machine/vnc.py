5be042b6544ff06e30a2d3af6159312ad3713e54
Ken Lett <kennric@osuosl.org>
2012-06-15 13:07:39 -0700

    Whitespace changes to prevent test hang
    refs #11427

--- ganeti_web/tests/views/virtual_machine/vnc.py
+++ ganeti_web/tests/views/virtual_machine/vnc.py
@@ -5,6 +5,7 @@ __all__ = ['TestVirtualMachineVNCViews']
 class TestVirtualMachineVNCViews(TestVirtualMachineViewsBase):
 
     def test_view_vnc(self):
+        pass
         """
         Tests view for cluster Ajax vnc (noVNC) script:
 
@@ -13,9 +14,11 @@ class TestVirtualMachineVNCViews(TestVirtualMachineViewsBase):
             * nonexistent Cluster returns 404
             * nonexistent VirtualMachine returns 404
         """
+
         url = "/cluster/%s/%s/vnc/"
         args = (self.cluster.slug, self.vm.hostname)
         self.validate_get(url, args, 'ganeti/virtual_machine/novnc.html')
+
 
     def test_view_vnc_proxy(self):
         """
@@ -27,11 +30,12 @@ class TestVirtualMachineVNCViews(TestVirtualMachineViewsBase):
             * nonexistent VirtualMachine returns 404
             * no ports set (not running proxy)
         """
-
         url = "/cluster/%s/%s/vnc_proxy/"
         args = (self.cluster.slug, self.vm.hostname)
 
         self.assert_standard_fails(url, args, method="post")
-        self.assert_200(url, args, [self.superuser, self.cluster_admin,
-                                    self.vm_admin], method="post",
+        self.assert_200(url, 
+                        args, 
+                        [self.superuser, self.cluster_admin, self.vm_admin], 
+                        method="post",
                         mime="application/json")
