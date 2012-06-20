cd3665d16136471ca83e7e92ea9d4ec35f35ca4d
Justin Noah <jnoah@osuosl.org>
2012-06-20 11:09:42 -0700

    Fixed tests
    refs #7791

--- ganeti_web/logs.py
+++ ganeti_web/logs.py
@@ -28,7 +28,8 @@ def build_vm_cache(user, object1, object2, object3, data):
     if object1 is not None:
         data['cluster_slug']=object1.cluster.slug
         data['hostname']=object1.hostname
-        data['newname'] = object1.newname
+        if hasattr(object1, 'newname'):
+            data['newname'] = object1.newname
         if object2 is not None:
             data['job_id'] = object2.job_id
     return data
