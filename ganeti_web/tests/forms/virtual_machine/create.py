4873f38d75adee1d762c3ac1ce622587a491a22a
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-09-26 15:56:52 -0700

    forms/vm: Implement checks for Ganeti 2.6 ipolicy limits, and test.

--- ganeti_web/tests/forms/virtual_machine/create.py
+++ ganeti_web/tests/forms/virtual_machine/create.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from ganeti_web import models
 from ganeti_web.models import Node
 from ganeti_web.forms.virtual_machine import (NewVirtualMachineForm,
+                                              VMWizardBasicsForm,
                                               VMWizardAdvancedForm)
 from ganeti_web.util.proxy import RapiProxy
 from ganeti_web.util.proxy.constants import INFO
@@ -13,11 +14,105 @@ from ganeti_web.tests.views.virtual_machine.base import (
 __all__ = [
     'TestNewVirtualMachineFormInit',
     'TestNewVirtualMachineFormValidation',
+    "TestVMWizardBasicsForm",
     "TestVMWizardAdvancedForm",
 ]
 
 Cluster = models.Cluster
 VirtualMachine = models.VirtualMachine
+
+
+class MockRapi(object):
+    """
+    Horrible mock.
+    """
+
+    def GetOperatingSystems(self):
+        return ["image+dobion-lotso"]
+
+
+class MockCluster(object):
+    """
+    Horrible mock.
+    """
+
+    info = {
+        "enabled_hypervisors": ["kvm", "lxc"],
+        "default_hypervisor": "kvm",
+        "beparams": {
+            "default": {
+                "maxmem": 256,
+                "vcpus": 1,
+            },
+        },
+        "software_version": "2.6.0",
+        "ipolicy": {
+            "max": {
+                "disk-size": 4096,
+                "memory-size": 1024,
+            },
+            "min": {
+                "disk-size": 1024,
+                "memory-size": 128,
+            },
+        },
+    }
+
+    rapi = MockRapi()
+
+
+
+class TestVMWizardBasicsForm(TestCase):
+
+    def setUp(self):
+        self.cluster = MockCluster()
+
+        self.valid_data = {
+            "hv": "kvm",
+            "os": "image+dobion-lotso",
+            "vcpus": 1,
+            "memory": 128,
+            "disk_template": "plain",
+            "disk_size": 2048,
+        }
+
+    def test_trivial(self):
+        pass
+
+    def test_validate_valid(self):
+        form = VMWizardBasicsForm(self.valid_data)
+        form._configure_for_cluster(self.cluster)
+        self.assertTrue(form.is_valid(), form.errors)
+
+    def test_validate_min_memory(self):
+        data = self.valid_data.copy()
+        data["memory"] = 64
+        form = VMWizardBasicsForm(data)
+        form._configure_for_cluster(self.cluster)
+        self.assertFalse(form.is_valid(), "Memory should be too small")
+
+    def test_validate_max_memory(self):
+        data = self.valid_data.copy()
+        data["memory"] = 2048
+        form = VMWizardBasicsForm(data)
+        form._configure_for_cluster(self.cluster)
+        self.assertFalse(form.is_valid(), "Memory should be too big")
+
+    def test_validate_min_disk_size(self):
+        data = self.valid_data.copy()
+        data["disk_size"] = 512
+        form = VMWizardBasicsForm(data)
+        form._configure_for_cluster(self.cluster)
+        self.assertFalse(form.is_valid(), "Disk size should be too small")
+
+    def test_validate_max_disk_size(self):
+        data = self.valid_data.copy()
+        data["disk_size"] = 16384
+        form = VMWizardBasicsForm(data)
+        form._configure_for_cluster(self.cluster)
+        self.assertFalse(form.is_valid(), "Disk size should be too big")
+
+
 
 class TestVMWizardAdvancedForm(TestCase):
 
@@ -41,6 +136,11 @@ class TestVMWizardAdvancedForm(TestCase):
             "pnode": self.pnode.id,
             "snode": self.snode.id,
         }
+
+    def tearDown(self):
+        self.pnode.delete()
+        self.snode.delete()
+        self.cluster.delete()
 
     def test_trivial(self):
         pass
