e96b655a387e089c5bf54e54cbb34c70c515890a
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-09-27 15:35:42 -0700

    muddle: Kill plugins.registerable.
    Unused.

--- muddle/__init__.py
+++ muddle/__init__.py
@@ -1,8 +1,5 @@
 from django.conf import settings as django_settings
 
-from muddle.plugins.registerable import PERM_ALL, PERM_NONE, PERM_READ, \
-                                    PERM_WRITE, PERM_CREATE, PERM_DELETE
-
 VERSION = '0.1'
 MUDDLE_ROOT = __file__[__file__.rfind('/')]
 
@@ -14,18 +11,4 @@ def settings_processor(request):
         'VERSION':VERSION,
         'STATIC':'%s/muddle_static' % django_settings.SITE_ROOT,
         'ROOT':django_settings.SITE_ROOT
-    }
-
-
-def perms_processor(request):
-    """
-    settings_processor adds default permission masks
-    """
-    return {
-        'PERM_ALL':PERM_ALL,
-        'PERM_NONE':PERM_NONE,
-        'PERM_READ':PERM_READ,
-        'PERM_WRITE':PERM_WRITE,
-        'PERM_CREATE':PERM_CREATE,
-        'PERM_DELETE':PERM_DELETE
     }
