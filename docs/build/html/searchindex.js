ceadfd8bfb683e8bfecab2b28c283501a7eab50c
Trevor Bramwell <bramwelt@osuosl.org>
2012-10-26 17:54:26 -0700

    Sphinx: Removed build files, as the directory gets autogenerated

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({objects:{},terms:{web:0,search:0,welcom:0,index:0,ganeti:0,modul:0,indic:0,manag:0,content:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Ganeti Web Manager&#8217;s documentation!"],objnames:{},filenames:["index"]})
