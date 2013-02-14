bab3419f0311e1078355ab9a026376e4e95b4fe2
Chance Zibolski <chance.zibolski@gmail.com>
2013-02-14 14:36:03 -0800

    Change disableSingletonDropdown to run on DOM ready
    Refactored the code to be more clear, and removed unnecessary deletion of page
    elements
    
    select element's CSS and ID now gets copied over to new element
    
    refs #enhancement/11781

--- ganeti_web/static/js/disableSingletonDropdown.js
+++ ganeti_web/static/js/disableSingletonDropdown.js
@@ -1,4 +1,4 @@
-function disableSingletonDropdown2() {
+$(function() {
     /*
      * Transform <select> elements with only one single option into labels and
      * hidden fields.
@@ -14,16 +14,22 @@ function disableSingletonDropdown2() {
 
     $("select").each(function(i, select) {
         if (select.options.length == 1) {
-            var option = select.options[0];
-            var hidden = $("<input type='hidden' />");
-            hidden.attr({
-                name: select.name,
-                value: option.value,
-            });
-            var span = $("<span />");
-            span.html(option.innerHTML);
-            $(select).after(hidden, span);
-            $(select).remove();
+            var $option = $(select.options[0]);
+            var $select = $(select);
+            var val = $option.val();
+            // select the option
+            $select.val(val)
+            // get the id/css/display text and apply it to our new element
+            var display = $option.html()
+            var id = $select.attr('id');
+            var classes = $select.attr('class');
+            var $element = $("<span />").attr('id', id)
+                                        .attr('class', classes)
+                                        .html(display);
+
+            // add our new element right after the select and hide the dropdown
+            $select.after($element);
+            $select.hide();
         }
     });
-}
+});
