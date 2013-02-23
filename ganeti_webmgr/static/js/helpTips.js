dcf12cdce3634f6d5bfb27468a3a149f7d9d803c
Chance Zibolski <chance.zibolski@gmail.com>
2013-02-22 19:46:11 -0800

    Add summary to end of VM Wizard
    Split the templates up a lot to use inheritance a bit better. Makes using a
    different template for different forms easier.
    
    Moved moved helptips out into its own file and made some changes to fix some
    CSS breakage I caused. Now the current helpTip() function works with the older
    style helptips, and the initHelpTip() function can probably removed.
    
    refs #11943

--- ganeti_webmgr/static/js/helpTips.js
+++ ganeti_webmgr/static/js/helpTips.js
@@ -1,3 +1,31 @@
+function helpTips(selector) {
+    // Inputs which have a help tip.
+    var $form_fields = $('input, select', selector);
+    var $tips = $('div[id^="help-"]');
+    // Hide help tips with javascript by default.
+    // With no js, the tips will still be there.
+    $tips.hide()
+    /* Binds the event to our inputs,
+       passing the focused element to the handler. */
+    $form_fields.live('focus', this, getHelpTips);
+
+    function getHelpTips(event) {
+        // The name of the input corresponds to the help tip's data-help attribute.
+        var help_name = this.name;
+        var $tip = $('#help-'+help_name);
+        // Fix for old helptip labels
+        if ($tip.children('h3').length == 0) {
+            var id = $(this).attr('id');
+            var label = $("label[for='" + id + "']").html();
+            $tip.prepend($("<h3>").html(label));
+        }
+        // Hide all tips.
+        $tips.hide()
+        // Show the tip.
+        $tip.show();
+    }
+}
+
 function initHelpTips(selector){
     /* initialize the help tips for each item on the create VM template */
 
@@ -6,7 +34,7 @@ function initHelpTips(selector){
             .end()
             .find('input[type="checkbox"]')
             .live('click', helpTip);
-    
+
     function helpTip(){
         var name = this.name;
         var label = $(this).prev('label').html();
@@ -20,7 +48,7 @@ function initHelpTips(selector){
         /* Strip all digits and underscores from end of name.
            Makes things work with unknown number of disks/nics. */
         var content = $('#help-'+name.replace(/[0-9_]*$/,''));
-            
+
         if(content.length != 0){
             $('#help')
                 .show();
