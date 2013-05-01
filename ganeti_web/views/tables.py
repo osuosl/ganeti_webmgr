11566b8f38d12ca1b67a44688bba19b84c93e2aa
Chance Zibolski <chance.zibolski@gmail.com>
2013-05-01 14:26:03 -0700

    Clean up tables
    refs #13407

--- ganeti_web/views/tables.py
+++ ganeti_web/views/tables.py
@@ -1,38 +1,36 @@
-from django.core.urlresolvers import reverse
-from django.utils.safestring import mark_safe
-
-import django_tables2 as tables
+from django_tables2 import Table, Column, LinkColumn, TemplateColumn
 from django_tables2.utils import A
 
 from ganeti_web.templatetags.webmgr_tags import (render_storage, render_os,
                                                  abbreviate_fqdn)
+from ganeti_web.utilities import hv_prettify
 
 
-class BaseVMTable(tables.Table):
+class BaseVMTable(Table):
 
-    # bulk = tables.CheckBoxColumn(accessor="pk", attrs={'name': 'bulk[]'})
-    status = tables.TemplateColumn(
+    status = TemplateColumn(
         template_name="ganeti/virtual_machine/vmfield_status.html",
     )
-    hostname = tables.LinkColumn(
+    hostname = LinkColumn(
         "instance-detail",
         kwargs={"cluster_slug": A("cluster.slug"),
                 "instance": A("hostname")},
         verbose_name='name',
     )
-    owner = tables.LinkColumn(
+    owner = LinkColumn(
         "user-detail-name",
         args=[A("owner")],
     )
-    primary_node = tables.Column(verbose_name='node')
-    operating_system = tables.Column(verbose_name='OS')
-    ram = tables.Column(verbose_name='RAM')
-    disk_size = tables.Column(verbose_name='disk space')
-    virtual_cpus = tables.Column(verbose_name='vCPUs')
+    primary_node = Column(verbose_name='node')
+    operating_system = Column(verbose_name='OS')
+    ram = Column(verbose_name='RAM')
+    disk_size = Column(verbose_name='disk space')
+    virtual_cpus = Column(verbose_name='vCPUs')
 
     class Meta:
         sequence = ("status", "hostname", "...")
         order_by = ("hostname")
+        empty_text = "No Virtual Machines"
 
     def __init__(self, *args, **kwargs):
         self.template = "table.html"
@@ -55,18 +53,14 @@ class VMTable(BaseVMTable):
 
     # This may look weird, but the VirtualMachine View shows clusters, but the
     # cluster view does not, which is why it isn't in the base table.
-    cluster = tables.Column()
+    cluster = LinkColumn(
+        "cluster-detail",
+        args=[A("cluster.slug")],
+        accessor="cluster.slug",
+        verbose_name='cluster'
+    )
 
     class Meta:
         sequence = ("status", "hostname", "cluster", "...")
         order_by = ("hostname")
 
-    # TODO: Make this into its own column type
-    def render_cluster(self, value, record):
-        text = abbreviate_fqdn(value)
-        if not self.can_create:  # no perms, dont make it a link.
-            return text
-        kwargs = {"cluster_slug": record.cluster.slug}
-        url = reverse("cluster-detail", kwargs=kwargs)
-        html = '<a href="{link}">{text}</a>'.format(link=url, text=text)
-        return mark_safe(html)
