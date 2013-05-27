1bfd534c8eb06e7a31d6fb6a4d1bb66c60a85173
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-05-27 11:13:04 -0500

    Removed some redundant imports in ganeti_web/views/graph.py Refs #13569

--- ganeti_web/views/graph.py
+++ ganeti_web/views/graph.py
@@ -1,36 +1,18 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.contrib.contenttypes.models import ContentType
 from django.http import (HttpResponse, HttpResponseRedirect,
                          HttpResponseForbidden, HttpResponseBadRequest,
                          Http404)
-from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.utils import simplejson as json
 from django.views.generic import TemplateView
 
-from object_log.views import list_for_object
-
-from object_permissions import get_users_any
-from object_permissions.signals import (view_add_user, view_edit_user,
-                                        view_remove_user)
-from object_permissions.views.permissions import view_users, view_permissions
-
-from object_log.models import LogItem
-log_action = LogItem.objects.log_action
-
-from ganeti_web.middleware import Http403
 from ganeti_web.models import Cluster, Node, VirtualMachine
-from ganeti_web.templatetags.webmgr_tags import render_storage
-from ganeti_web.util.client import GanetiApiError
-from ganeti_web.utilities import (cluster_os_list, compare, os_prettify,
-                                  get_hypervisor)
-from ganeti_web.views.generic import (NO_PRIVS, LoginRequiredMixin,
-                                      PagedListView)
+from ganeti_web.views.generic import LoginRequiredMixin
 
 
 
-def vmqs_to_dicts(vm_queryset):
+def vmqs_to_dicts(vm_queryset,node_queryset):
     '''
     (Virtual Machine Queryset to Dictionaries)
     A function that converts a queryset of virtual machines belonging to a cluster to relevant structures.
@@ -41,6 +23,10 @@ def vmqs_to_dicts(vm_queryset):
     '''
     nodedict = {}
     psdict = {}
+    for node in node_queryset:
+        nodedict[node] = []
+        psdict[node] = {}
+
     for vm_obj in vm_queryset:
         vm = vm_obj.hostname
         pnode = vm_obj.primary_node
@@ -94,6 +80,7 @@ def js_edges_obj(nodedict,psdict):
         #Edges to Instances.
         for instance in nodedict[node]:
             s += '\t\t"%s":{length:6},\n'%(instance,)
+
         #Edges to Secondary Nodes.
         for snode,slinkweight in psdict[node].items():
             if snode:
@@ -114,9 +101,9 @@ class ClusterGraphView(LoginRequiredMixin, TemplateView):
         context = super(ClusterGraphView, self).get_context_data(** kwargs)
 
         cluster = Cluster.objects.get(slug=self.request.GET['cluster'])
-        nodes = Node.objects.filter(cluster=cluster)
+        nodeqs = Node.objects.filter(cluster=cluster)
         vmqs = VirtualMachine.objects.filter(cluster=cluster)
-        nodedict,psdict = vmqs_to_dicts(vmqs)
+        nodedict,psdict = vmqs_to_dicts(vmqs,nodeqs)
 
         graph_nodes = js_nodes_obj(nodedict)
         graph_edges = js_edges_obj(nodedict,psdict)
