a155f1e8fb05ddce43e3b49e58ad4e3c1bfbad3a
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-07-15 21:06:32 +0530

    Added urls to ganetiviz app Refs #13569

---
+++ ganeti_web/ganetiviz/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import patterns, url
+from ganetiviz.views import VMJsonView,NodeJsonView
+
+urlpatterns += patterns(
+    'ganetiviz.views',
+
+    url(r'^ganetiviz/vms/(?P<cluster_hostname>[\.\w]+)$', VMJsonView.as_view(), 
+        name='json-vms'),
+
+    url(r'^ganetiviz/nodes/(?P<cluster_hostname>[\.\w]+)$', NodeJsonView.as_view(),
+        name='json-nodes'),
+)
