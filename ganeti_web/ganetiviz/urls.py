ddbbc3c2fcb094f6a79dd33264df8357a239f8b2
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-07-17 11:16:40 +0530

    Added urlconf entry in ganetiviz/urls for ClusterGraphView Refs #13569

--- ganeti_web/ganetiviz/urls.py
+++ ganeti_web/ganetiviz/urls.py
@@ -9,4 +9,7 @@ urlpatterns += patterns(
 
     url(r'^ganetiviz/nodes/(?P<cluster_hostname>[\.\w]+)$', NodeJsonView.as_view(),
         name='json-nodes'),
+
+    url(r'^map/(?P<cluster_hostname>[\.\w]+)$', ClusterGraphView.as_view(), 
+        name='cluster-graph'),
 )
