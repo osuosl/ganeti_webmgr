9cadc28ea90e8b342ec9026422cd87ae96db8b3a
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-09-12 15:04:27 +0530

    Added testcase in ganetiviz/tests that verifies format of cluster json data returned
    refs #14967

--- ganeti_web/ganetiviz/tests/views.py
+++ ganeti_web/ganetiviz/tests/views.py
@@ -10,57 +10,39 @@ from clusters.models import Cluster
 from nodes.models import Node
 from virtualmachines.models import VirtualMachine
 
+__all__ = ['TestGanetivizViews', ]
 
-testcluster0_nodes = [{'pk': 1, 'model': 'nodes.node',
-  'fields': {'ram_free': 1111, 'offline': False,
-             'hostname': 'node0.example.test', 'role': 'M', 'ram_total': 9999}},
- {'pk': 2, 'model': 'nodes.node',
-  'fields': {'ram_free': 1111, 'offline': False,
-             'hostname': 'node1.example.test', 'role': 'M', 'ram_total': 9999}}]
+testcluster0_data = {'nodes': [{'hostname': 'node0.example.test',
+            'offline': False,
+            'ram_free': -1,
+            'ram_total': -1,
+            'role': u''},
+           {'hostname': 'node1.example.test',
+            'offline': False,
+            'ram_free': -1,
+            'ram_total': -1,
+            'role': u''}],
+ 'vms': [{'hostname': 'instance1.example.test',
+          'owner': None,
+          'primary_node__hostname': 'node0.example.test',
+          'secondary_node__hostname': 'node1.example.test',
+          'status': u''},
+         {'hostname': 'instance2.example.test',
+          'owner': None,
+          'primary_node__hostname': 'node0.example.test',
+          'secondary_node__hostname': 'node1.example.test',
+          'status': u''},
+         {'hostname': 'instance3.example.test',
+          'owner': None,
+          'primary_node__hostname': 'node0.example.test',
+          'secondary_node__hostname': 'node1.example.test',
+          'status': u''},
+         {'hostname': 'instance4.example.test',
+          'owner': None,
+          'primary_node__hostname': 'node1.example.test',
+          'secondary_node__hostname': 'node0.example.test',
+          'status': u''}]}
 
-testcluster0_vms = [{'fields': {'hostname': 'instance1.example.test',
-             'minram': -1,
-             'operating_system': 'image+gentoo-hardened-cf',
-             'owner': None,
-             'primary_node': None,
-             'ram': 512,
-             'secondary_node': None,
-             'status': 'running'},
-  'model': 'virtualmachines.virtualmachine',
-  'pk': 1},
- {'fields': {'hostname': 'instance2.example.test',
-             'minram': -1,
-             'operating_system': 'image+gentoo-hardened-cf',
-             'owner': None,
-             'primary_node': None,
-             'ram': 512,
-             'secondary_node': None,
-             'status': 'running'},
-  'model': 'virtualmachines.virtualmachine',
-  'pk': 2},
- {'fields': {'hostname': 'instance3.example.test',
-             'minram': -1,
-             'operating_system': 'image+gentoo-hardened-cf',
-             'owner': None,
-             'primary_node': None,
-             'ram': 512,
-             'secondary_node': None,
-             'status': 'running'},
-  'model': 'virtualmachines.virtualmachine',
-  'pk': 3},
- {'fields': {'hostname': 'instance4.example.test',
-             'minram': -1,
-             'operating_system': 'image+gentoo-hardened-cf',
-             'owner': None,
-             'primary_node': None,
-             'ram': 512,
-             'secondary_node': None,
-             'status': 'running'},
-  'model': 'virtualmachines.virtualmachine',
-  'pk': 4}]
-
-
-@unittest.skipIf(True, "Skipping non updated Visualization Tests")
 class TestGanetivizViews(TestCase, ViewTestMixin, UserTestMixin):
     def setUp(self):
         user = User(id=2, username='tester_pranjal')
@@ -127,26 +109,14 @@ class TestGanetivizViews(TestCase, ViewTestMixin, UserTestMixin):
         Node.objects.all().delete()
         Cluster.objects.all().delete()
 
-    @unittest.skip("Skipping non updated Visualization Tests")
-    def test_nodes_json_ouput(self):
-        url = "/ganetiviz/nodes/%s/"
+    def test_cluster_json_ouput(self):
+        url = "/ganetiviz/cluster/%s/"
         args = self.cluster.slug
 
         self.c.login(username='tester_pranjal', password='secret')
         response = self.c.get(url % args)
         content = response.content
-        nodes_json = json.loads(content)
+        cluster_data = json.loads(content)
 
-        self.assertEqual(nodes_json,testcluster0_nodes)
+        self.assertEqual(cluster_data,testcluster0_data)
 
-    @unittest.skip("Skipping non updated Visualization Tests")
-    def test_vms_json_ouput(self):
-        url = "/ganetiviz/vms/%s/"
-        args = self.cluster.slug
-
-        self.c.login(username='tester_pranjal', password='secret')
-        response = self.c.get(url % args)
-        content = response.content
-        vms_json = json.loads(content)
-
-        self.assertEqual(vms_json,testcluster0_vms)
