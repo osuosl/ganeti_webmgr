288e34c2c52ea1f85aff3317d95e9f7b2deada40
Chance Zibolski <chance.zibolski@gmail.com>
2014-05-28 12:33:42 -0700

    wsgi.py: Remove unnecessary additional paths

--- ganeti_webmgr/ganeti_web/wsgi.py
+++ ganeti_webmgr/ganeti_web/wsgi.py
@@ -1,12 +1,6 @@
 import os
-from os.path import abspath, dirname
-from sys import path
 
-CURRENT_DIR = dirname(abspath(__file__))
-APP_ROOT = dirname(CURRENT_DIR)
-path.append(APP_ROOT)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ganeti_web.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ganeti_webmgr.ganeti_web.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
