938c7a0b3c560f65820ab23cfd6d21e7e74fe95d
Chance Zibolski <chance.zibolski@gmail.com>
2014-05-28 12:42:43 -0700

    manage.py: Use full package path to settings

--- ganeti_webmgr/ganeti_web/wsgi.py
+++ ganeti_webmgr/ganeti_web/wsgi.py
@@ -1,6 +1,7 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ganeti_webmgr.ganeti_web.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "ganeti_webmgr.ganeti_web.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
