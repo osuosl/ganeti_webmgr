6b54411b11bafced6bc7262df46523d6354856b1
Ian Kronquist <iankronquist@teleport.com>
2015-05-19 14:36:46 -0700

    Tell user the consequences of not including docs

--- scripts/travis-check-docs.bash
+++ scripts/travis-check-docs.bash
@@ -7,11 +7,13 @@ DOC_PATTERNS_LIST=( 'docs/' 'README' 'CHANGELOG' )
 CHANGED_FILES=`git rev-list HEAD ^$PARENT_BRANCH | xargs git show --pretty="format:" --name-only`
 
 for PATTERN in "${DOC_PATTERNS_LIST[@]}"; do
-	echo next pattern
-	echo $PATTERN
 	if [[ $CHANGED_FILES =~ $PATTERN ]]; then
+		echo "Yay, you wrote some docs! Here's a gold star: 🌟 "
 		exit 0
 	fi
 done
+
+echo "No docs were included since you branched off $PARENT_BRANCH. That's okay,
+but you should justify why you don't need docs in the PR."
 
 exit 1
